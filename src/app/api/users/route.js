b376501e4c0b354b1016b2b8ed8b21ec6db66add
rafael-the-dev <luislangabusiness@gmail.com>
2024-04-05 22:25:34 +0200

    create POST method handler

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,10 +1,24 @@
 import { NextResponse } from "next/server";
 
-import { apiHandler } from "@/middlewares/route-handler"
+import Users from "@/models/server/db/Users";
+
+import { apiHandler } from "@/middlewares/route-handler";
 
 export const GET = async () => {
     return await apiHandler(async ({ mongoDbConfig, user }) => {
-        const users = await mongoDbConfig.collections.USERS.find({}).toArray();
+        const users = await Users.getAll({}, { mongoDbConfig, user });
         return NextResponse.json(users);
     });
+};
+
+
+export const POST = async (req) => {
+    const { category, firstName, lastName, password, username } = await req.json();
+
+    return await apiHandler(
+        async ({ mongoDbConfig, user }) => {
+            await Users.register({  category, firstName, lastName, password, username }, { mongoDbConfig, user });
+            return NextResponse.json({}, { status: 201 });
+        }
+    )
 };
