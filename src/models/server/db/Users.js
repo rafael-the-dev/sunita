1070577eca738dfb6effda84df33b55a07b00168
rafael-the-dev <luislangabusiness@gmail.com>
2024-04-05 22:26:35 +0200

    create Users register method to save user's data in the db

---
+++ src/models/server/db/Users.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+
+import { getId } from "@/helpers/id"
+
+class Users {
+
+    static async getAll({}, { mongoDbConfig }) {
+        const users = await mongoDbConfig.collections.USERS.find({}).toArray();
+        return users;
+    }
+
+    static async register({ category, firstName, lastName, password, username }, { mongoDbConfig }) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await mongoDbConfig.collections.USERS
+            .insertOne({ 
+                category,
+                firstName, 
+                lastName, 
+                password: hashedPassword,
+                username 
+            });
+    }
+}
+
+export default Users;
