bed960c12fa30e507086879372536f1b4adbc84a
rafael-the-dev <luislangabusiness@gmail.com>
2024-04-09 13:37:56 +0200

    implement errors handler

--- src/models/server/db/Auth.js
+++ src/models/server/db/Auth.js
@@ -4,29 +4,38 @@ import jwt from "jsonwebtoken";
 import { decode, verifyToken } from "@/helpers/auth";
 
 import AuthError from "@/errors/server/AuthenticationError";
+import Error404 from "@/errors/server/404Error";
+
 import Users  from "./Users";
 
 class Auth {
 
     static async login({ password, username }, { mongoDbConfig }) {
-        const user = await Users.get({ username }, { mongoDbConfig });
-        const match = await bcrypt.compare(password, user.password);
+        try {
+            const user = await Users.get({ username }, { mongoDbConfig });
+        
+            const match = await bcrypt.compare(password, user.password);
 
-        if(match) {
-            const token = decode(user);
+            if(match) {
+                const token = decode(user);
 
-            const { _id, exp, iat, password, ...userDetails } = verifyToken(token);
+                const { _id, exp, iat, password, ...userDetails } = verifyToken(token);
 
-            return { 
-                access: {
-                    expiresIn: exp,
-                    token 
-                },
-                data: userDetails
-            };
+                return { 
+                    access: {
+                        expiresIn: exp,
+                        token 
+                    },
+                    data: userDetails
+                };
+            }
+
+            throw new AuthError("Username or password invalid.");   
+        } catch(e) {
+            if(e instanceof Error404) throw new AuthError("Username or password invalid.");   
+
+            throw e;
         }
-
-        throw new AuthError("Username or password invalid.");
     }
 
     static async refreshToken({ token }) {
