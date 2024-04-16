2f4e6915d0ee3ad5fc3c52ac1dadc6a9c6400c9b
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-16 17:21:47 +0100

    convert link component to typescript

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,7 +1,14 @@
+import * as React from "react";
 import Link from "next/link";
 import Typography from "@mui/material/Typography";
 
-const LinkContainer = ({ className, children, href , ...rest}) => (
+type Props = {
+    className?: string;
+    children: React.ReactNode;
+    href: string;
+}
+
+const LinkContainer = ({ className, children, href , ...rest}: Props) => (
     <Typography 
         { ...rest }
         component={Link} 
