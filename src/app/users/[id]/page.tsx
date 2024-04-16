53b900e6a52ffe3ee947af922f0d9598f0f9cb93
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-16 17:51:31 +0100

    move users/[username]'s menu component to Header component

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -4,17 +4,13 @@ import { useContext } from "react";
 import { useParams } from "next/navigation";
 import Hidden from "@mui/material/Hidden";
 
-import Menu from "./components/menu";
-
 const Container = () => {
     const { id } = useParams();
 
     return (
         <main>
             <div>
-                <Hidden mdUp>
-                    <Menu />
-                </Hidden>
+                
             </div>
         </main>
     );
--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,23 +1,28 @@
 
 import classNames from "classnames";
 
-import Avatar from "@mui/material/Avatar"
+import Avatar from "@mui/material/Avatar";
+import Hidden from "@mui/material/Hidden";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
 
+import styles from "./styles.module.css";
+
+import Menu from "./components/menu";
 import UserProfile from "./components/user-profile";
 
 const Header = () => {
 
     return (
-        <header className="bg-primary-100 flex items-center justify-between px-4 py-2">
-            <div>
+        <header className="bg-primary-100 flex items-center justify-between px-1 py-2 md:px-4">
+            <div className="flex grow items-center">
+                <Hidden mdUp><Menu /></Hidden>
                 <Typography
                     component="h1"
-                    className={classNames("font-bold text-xl")}>
+                    className={classNames(styles.title, "font-bold overflow-hidden text-xl text-ellipsis text-nowrap")}>
                     Dashboard
                 </Typography>
             </div>
