eeead17bf8d43d12fd7e5427ccfedd5051aa66a9
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-22 06:32:28 +0100

    retrieve and render products

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/index.tsx
@@ -1,28 +1,68 @@
 import { useContext } from "react";
+import { useFormState } from "react-dom";
 import classNames from "classnames";
 
 import TextField from "@mui/material/TextField";
 
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
+
+import { AppContext } from "@/context/AppContext";
+import { LoginContext } from "@/context/LoginContext";
+import { ProductType } from "@/types/product";
 
 import Collapse from "@/components/shared/collapse"
 import Categories from "@/components/shared/categories";
 import SubmitButton from "@/components/shared/submit-button";
+import currency from "currency.js";
+
+type FormstateType = {
+
+};
 
 const Body = () => {
+    const { dialog, isLoading } = useContext(AppContext);
+    const { user } = useContext(LoginContext)
+
+    const submitHandler = async (prevState, formData) => {
+        const product: ProductType = {
+            barcode: formData.get("barcode-input"),
+            category: "t-shirt",
+            id: "123456789",
+            name: formData.get("name-input"),
+            purchasePrice: currency(formData.get("purchase-price-input")).value,
+            sellPrice: currency(formData.get("sell-price-input")).value
+        };
+
+        isLoading.current = true;
+
+        try {
+            const body = JSON.stringify(product);
+
+            await fetch(`/api/users/${"rafaeltivane"}/warehouses/12345/products`, { method: "POST", body })
+        } catch(e) {
+            console.error(e)
+        } finally {
+            isLoading.current = false;
+        }
+    };
+
+    const [ state, formAction ] = useFormState(submitHandler, { hasErrors: false });
 
     return (
         <form 
+            action={formAction}
             className={classNames(styles.form, `flex flex-col justify-between`)}>
             <div className={classNames(styles.formContent, styles.spacing, `grow overflow-y-auto`)}>
                 <div className="md:flex justify-between flex-wrap">
                     <TextField
                         className={classNames(styles.formInput)}
+                        name="name-input"
                         placeholder="Name"
                         label="Name"
                     />
                     <TextField
                         className={classNames(styles.formInput)}
+                        name="barcode-input"
                         placeholder="Barcode"
                         label="Barcode"
                     />
@@ -35,15 +75,18 @@ const Body = () => {
                 </div>
                 <Collapse
                     classes={{ root: "border border-primary-300 border-solid rounded-md" }}
+                    highlightOnOpen
                     title="Price">
                     <div className="md:flex justify-between flex-wrap rounded">
                         <TextField
                             className={classNames(styles.formInput)}
+                            name="purchase-price-input"
                             placeholder="Purchase price"
                             label="Purchase price"
                         />
                         <TextField
                             className={classNames(styles.formInput)}
+                            name="sell-price-input"
                             placeholder="Sell price"
                             label="Sell price"
                         />
@@ -51,7 +94,9 @@ const Body = () => {
                 </Collapse>
             </div>
             <div className={classNames("flex justify-end mt-4", styles.spacing)}>
-                <SubmitButton>Submit</SubmitButton>
+                <SubmitButton>
+                    { dialog?.payload ? "Update" : "Submit"  }
+                </SubmitButton>
             </div>
         </form>
     );
--- src/app/users/[id]/warehouses/[warehouseId]/products/page.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/page.tsx
@@ -1,13 +1,74 @@
+"use client";
+
 import * as React from "react";
 import classNames from "classnames";
 
 import styles from "./styles.module.css";
 
+import { ProductInfoType } from "@/types/product";
+
 import Main from "@/components/main";
 import RegisterProduct from "./components/register-product";
 import SearchBox from "@/components/shared/product-search-box";
+import Table from "@/components/shared/table";
+import { TableHeadersType } from "@/components/table/types";
+
 
 const Container = () => {
+    const [ productsList, setProductsList ] = React.useState<ProductInfoType[] | null>(null);
+
+    const headers = React.useRef<TableHeadersType[]>([
+        {
+            label: "Name",
+            key: {
+                value: "name"
+            }
+        },
+        {
+            label: "Barcode",
+            key: {
+                value: "barcode"
+            }
+        },
+        {
+            label: "Category",
+            key: {
+                value: "category"
+            }
+        },
+        {
+            label: "Price",
+            key: {
+                value: "price"
+            }
+        },
+        {
+            label: "Quantity",
+            key: {
+                value: "stock",
+                subKey: {
+                    value: "quantity"
+                }
+            }
+        }
+    ]);
+
+    const fetchData = React.useCallback(async ({ signal }: { signal: AbortSignal }) => {
+        const res = await fetch("http://localhost:3000/api/users/rafaeltivane/warehouses/12345/products", { signal });
+        const data = await res.json() as ProductInfoType[];
+        setProductsList(data);
+    }, []);
+
+    React.useEffect(() => {
+        const controller = new AbortController();
+
+        fetchData({ signal: controller.signal});
+
+        return () => {
+            controller.abort;
+        }
+        
+    }, [ fetchData ])
 
     return (
         <Main className="flex flex-col items-stretch justify-between">
@@ -21,8 +82,11 @@ const Container = () => {
                         />
                     </SearchBox>
                 </form>
-                <div className={classNames(styles.body, `table-body`)}>
-
+                <div className={classNames(styles.body, `mt-6 table-body`)}>
+                    { productsList && <Table 
+                        data={productsList}
+                        headers={headers}
+                    /> }
                 </div>
             </div>
             <div className="flex justify-end px-3">
