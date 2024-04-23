0aa0f0e5778324705be363c2bdeeb0cbf8f28f5d
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-23 18:10:55 +0100

    hightlight collapse's button base on highlightOnOpen state

--- src/components/shared/collapse/index.tsx
+++ src/components/shared/collapse/index.tsx
@@ -9,11 +9,12 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 type CollapseProps = { 
     children: ReactNode, 
     classes?: { root?: string }; 
+    highlightOnOpen?: boolean;
     open?: boolean, 
     title: string 
 };
 
-const Container = ({ children, classes, open, title }: CollapseProps) => {
+const Container = ({ children, classes, highlightOnOpen, open, title }: CollapseProps) => {
     const [ state, setState ] = useState(Boolean(open));
 
     const childrenMemo = useMemo(() => children, [ children ])
@@ -23,7 +24,10 @@ const Container = ({ children, classes, open, title }: CollapseProps) => {
     return (
         <div className={classNames(classes?.root)}>
             <Button 
-                className="capitalize font-semibold justify-between text-primary-800 shadow-none w-full hover-secondary-button"
+                className={classNames(
+                    "capitalize font-semibold justify-between text-primary-800 shadow-none w-full hover-secondary-button",
+                    { "bg-primary-800 opacity-30 rounded-b-none text-white": highlightOnOpen }
+                )}
                 endIcon={ state ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon /> }
                 onClick={clickHandler}>
                 { title }
