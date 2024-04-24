2f19e3630942d4645c2d0ff99031cc1763371b30
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-24 18:43:26 +0100

    remove generics type

--- src/components/shared/table/index.tsx
+++ src/components/shared/table/index.tsx
@@ -5,28 +5,28 @@ import Table from "@/components/table";
 import TableRow from "@/components/table/components/table-row";
 import { TableClassesType, TableHeadersType } from "@/components/table/types";
 
-type TablePropsType<T> = {
+type TablePropsType = {
     classes?: TableClassesType;
     headers: React.MutableRefObject<TableHeadersType[]>
-    data: T[];
-    onClickRow?: (row: T) => (e: React.MouseEvent<HTMLTableRowElement>) => void;
-    onRemoveRow?: (row: T) => (e: React.MouseEvent<HTMLButtonElement>) => void;
+    data: Object[];
+    onClickRow?: (row: Object) => (e: React.MouseEvent<HTMLTableRowElement>) => void;
+    onRemoveRow?: (row: Object) => (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const TableContainer = <T extends Object>({ classes, data, headers, onClickRow, onRemoveRow }: TablePropsType<T>) => {
+const TableContainer = ({ classes, data, headers, onClickRow, onRemoveRow }: TablePropsType) => {
     
     const getBodyRows = ({ page, rowsPerPage }: { page: number, rowsPerPage: number }) => {
         const list = rowsPerPage > 0 ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : data;
 
         return list.map(row => (
-            <TableRow 
-                headers={headers}
-                key={uuidV4()}
-                onClick={onClickRow}
-                onRemove={onRemoveRow}
-                row={row}
-            />
-        ))
+                <TableRow 
+                    headers={headers}
+                    key={uuidV4()}
+                    onClick={onClickRow}
+                    onRemove={onRemoveRow}
+                    row={row}
+                />
+            ))
     };
 
     return (
