10d192605efcb7432bedac5a5b8aa37c67dfd4a8
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-25 08:49:12 +0100

    create payment method types

---
+++ src/types/payment-method.ts
@@ -0,0 +1,12 @@
+
+
+export type PaymentMethodType = {
+    receivedAmount: number;
+    id: string | number;
+    amount: number;
+}
+
+export type PaymentMethodListItemType = {
+    label: string;
+    value: string | number
+}
