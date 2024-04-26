9c04a9014629acc5f6ed517745d9ab50189bb698
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-26 12:47:01 +0100

    add sales field

--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -1,6 +1,8 @@
 
 import { WarehouseProductType } from "./product"
+import { SaleType } from "./sale";
 
 export type WarehouseType = {
     products: WarehouseProductType[];
+    sales: SaleType[];
 };
