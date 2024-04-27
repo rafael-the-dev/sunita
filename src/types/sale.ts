d0863a52632c13da6664ae464cabf02437b234cf
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-27 13:28:50 +0100

    create sales info type

--- src/types/sale.ts
+++ src/types/sale.ts
@@ -1,12 +1,27 @@
+import { ProductInfoType } from "./product";
 
 
 export type SaleType = {
     changes: number;
     createAt: Date;
     id: string;
-    products: {
+    profit: number;
+    items: {
         id: string;
         price: number;
+        quantity: number;
+    }[];
+    total: number;
+    totalReceived: number;
+    user: string;
+}
+
+export type SaleInfoType = {
+    changes: number;
+    createAt: Date;
+    id: string;
+    items: {
+        product: ProductInfoType;
         quantity: number;
     }[];
     total: number;
