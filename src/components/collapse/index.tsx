7e4a562e3577e76ba16622402c81e3e65df27a7b
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-29 16:30:02 +0100

    add collapse search param, if showSearchParam is true

--- src/components/collapse/index.tsx
+++ src/components/collapse/index.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
 import Collapse from "@mui/material/Collapse";
+import useSearchParamsHook from "@/hooks/useSearchParams";
 
 type CollapsePropsType = {
     children: React.ReactNode,
     onClose?: React.MutableRefObject<() => void>;
     onOpen?: React.MutableRefObject<() => void>;
-    onToggle?: React.MutableRefObject<() => void>
+    onToggle?: React.MutableRefObject<() => void>;
+    showSearchParam?: boolean;
 };
 
-const CollapseContainer = ({ children, onClose, onOpen, onToggle, ...rest }: CollapsePropsType) => {
+const CollapseContainer = ({ children, onClose, onOpen, onToggle, showSearchParam, ...rest }: CollapsePropsType) => {
     const [ open, setOpen ] = React.useState(false);
 
     const childrenMemo = React.useMemo(() => children, [ children ]);
+
+    const searchParams = useSearchParamsHook();
+    React.useEffect(() => {
+        if(showSearchParam) {
+            searchParams.setSearchParam("collapse", JSON.stringify(open))
+        }
+    }, [ open, searchParams, showSearchParam ])
 
     React.useEffect(() => {
         if(Boolean(onClose)) onClose.current = () => setOpen(false);
