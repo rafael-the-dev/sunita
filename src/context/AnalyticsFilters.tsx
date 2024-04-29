b037f3fe29cad01a90829a5acdcdf0e222381abe
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-29 16:28:48 +0100

    refactor codd

--- src/context/AnalyticsFilters.tsx
+++ src/context/AnalyticsFilters.tsx
@@ -1,12 +1,14 @@
 import useFech from "@/hooks/useFetch";
 import { ProductInfoType } from "@/types/product";
+import { UserType } from "@/types/user";
 import * as React from "react";
 
 
 type AnalyticsFiltersType = {
     getCategories: () => any[];
-    getProducts: () => ProductInfoType[];
+    getData: <T extends Object>(key: string) => T;
     onToggleCollapse: React.MutableRefObject<() => void>;
+    setData: <T extends Object>(key: string, data: T) => void;
 }
 
 export const AnalyticsFiltersContext = React.createContext<AnalyticsFiltersType>(null);
@@ -17,25 +19,30 @@ type PropsType = {
     children: React.ReactNode;
 }
 
+type PropertiesType = {
+    products: ProductInfoType[];
+    users: UserType[];
+}
+
 export const AnalyticsFiltersContextProvider = ({ autoFetchProducts= true,children }: PropsType) => {
+    const properties = React.useRef<PropertiesType>({
+        products: [],
+        users: []
+    });
 
     const onToggleCollapse = React.useRef<() => void | null>(null)
 
-    const fetchProductsResult = useFech<ProductInfoType[]>({ 
-            autoFetch: autoFetchProducts, 
-            url: "/api/users/rafaeltivane/warehouses/12345/products" 
-        }
-    );
-
     const getCategories = React.useCallback(() => [], [])
-    const getProducts = React.useCallback(() => fetchProductsResult.data, [ fetchProductsResult ]);
+
+    const getData = React.useCallback(<T extends Object>(key: string): T => properties.current[key], []);
+    const setData = React.useCallback(<T extends Object>(key: string, data: T) => { properties.current[key] = data }, []);
 
     return (
         <AnalyticsFiltersContext.Provider
             value={{
-                getCategories,
-                getProducts,
-                onToggleCollapse
+                getCategories, getData,
+                onToggleCollapse,
+                setData,
             }}>
             { children }
         </AnalyticsFiltersContext.Provider>
