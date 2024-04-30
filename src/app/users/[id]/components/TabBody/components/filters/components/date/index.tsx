54ef92c6fe6ea7079e065af49af9fc82dea54161
Rafael Tivane <luislangabusiness@gmail.com>
2024-04-30 17:48:37 +0100

    use custom date-filter component

--- src/app/users/[id]/components/TabBody/components/filters/components/date/index.tsx
+++ src/app/users/[id]/components/TabBody/components/filters/components/date/index.tsx
@@ -1,45 +1,31 @@
-import * as React from "react";
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
 import classNames from "classnames";
-import moment from "moment"
 
 import classes from "./styles.module.css";
 import useSearchParams from "@/hooks/useSearchParams";
-import { dateTimeFormat, toISOString } from "@/helpers/date";
+import { toISOString } from "@/helpers/date";
 
-import Input from "@/components/Textfield";
+import DateTimeInput from "@/components/shared/date-filter";
 
 const DateFilters = () => {
     const searchParams = useSearchParams()
 
     const startDate = searchParams.get("start-date", "");
-    const endDate = searchParams.get("end-date", "")
-
-    const changeHandler = React.useCallback((key: string) => (value: string) => {
-        searchParams.setSearchParam(key, toISOString(value));
-    }, [ searchParams ]);
 
     return (
         <div className="flex flex-wrap items-center">
-            <DesktopDateTimePicker
+            <DateTimeInput
                 className={classNames(classes.datePicker, "input sm:mr-4 sm:mb-0")}
-                inputFormat={dateTimeFormat}
+                id="start-date"
                 label="Date"
-                value={startDate ?? moment(Date.now())}
-                minDate={toISOString('2024-01-01 00:00:00')}
-                onChange={changeHandler("start-date")}
-                renderInput={(params) => <Input {...params} />}
             />
-            { Boolean(startDate) && <DesktopDateTimePicker
-                className={classNames(classes.datePicker, "input sm:mr-4 sm:mb-0")}
-                inputFormat={dateTimeFormat}
-                label="End date"
-                value={endDate}
-                minDate={toISOString(startDate)}
-                onChange={changeHandler("end-date")}
-                renderInput={(params) => <Input {...params} />}
-            /> }
+            { Boolean(startDate) && (
+                <DateTimeInput
+                    className={classNames(classes.datePicker, "input sm:mr-4 sm:mb-0")}
+                    id="end-date"
+                    label="End date"
+                    minDate={toISOString(startDate)}
+                /> 
+            )}
         </div>
     );
 };
