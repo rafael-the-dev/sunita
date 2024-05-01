7e5ee33925f76c6198bb86d02a2d1c3893fc4b0a
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-01 06:34:02 +0100

    add screen resolution

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,22 @@ module.exports = {
             },
             colors: {
                 primary: colors.zinc
+            },
+            screens: {
+                'sm': '600px',
+                // => @media (min-width: 640px) { ... }
+
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+
+                'lg': '900px',
+                // => @media (min-width: 1024px) { ... }
+
+                'xl': '1024px',
+                // => @media (min-width: 1280px) { ... }
+
+                '2xl': '1280px',
+                // => @media (min-width: 1536px) { ... }
             }
         },
   },
