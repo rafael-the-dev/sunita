e53ac58c2ea81205caaf05a3c0e5204c7761541d
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-01 07:38:47 +0100

    create new Expense type, and add it to warehouse type

---
+++ src/types/expense.ts
@@ -0,0 +1,40 @@
+
+
+export type ExpenseItemType = {
+    description: string;
+    id: string;
+    price: number;
+}
+
+export enum ExpenseStatus {
+    "SUCCESSFULL",
+    "UNSUCCESSFULL"
+}
+
+export type ExpenseClientType = {
+    category: string;
+    items: ExpenseItemType[],
+    total: number;
+}
+
+export type ExpenseType = {
+    category: string;
+    createdAt: string | Date;
+    items: ExpenseItemType[],
+    status: ExpenseStatus;
+    total: number;
+    user: string;
+}
+
+export type ExpenseInfoType = {
+    category: string;
+    createdAt: string | Date;
+    items: ExpenseItemType[],
+    status: ExpenseStatus;
+    total: number;
+    user: {
+        firstName: string;
+        lastName: string;
+        username: string;
+    };
+}
--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -1,8 +1,10 @@
 
+import { ExpenseType } from "./expense";
 import { WarehouseProductType } from "./product"
 import { SaleType } from "./sale";
 
 export type WarehouseType = {
+    expenses: ExpenseType[];
     products: WarehouseProductType[];
     sales: SaleType[];
 };
