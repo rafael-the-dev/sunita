42f51cfc6eb68d9a1b35ffd8b2a8ad45bb900661
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-02 18:12:22 +0100

    update, delete and return sorted expenses by date

--- src/models/server/db/Expenses.ts
+++ src/models/server/db/Expenses.ts
@@ -8,20 +8,37 @@ import { WarehouseType } from "@/types/warehouse";
 import InvalidArgumentError from "@/errors/server/InvalidArgumentError";
 import Store from "./Warehouse";
 import { getId } from "@/helpers/id";
+import Error404 from "@/errors/server/404Error";
 
 type GetAllPropsType = {
     filters?: Object;
     storeId: string;
-}
+};
 
 type PropsType = {
     expense: ExpenseClientType;
     storeId: string;
 }
 
+type DeletePropsType = {
+    expenseId: string;
+    storeId: string;
+}
+
+type UpdatePropsType = PropsType & {
+    expenseId: string;
+}
+
+
+type UpdateStorePropsType = {
+    helper: (store: WarehouseType) => ExpenseType[];
+    storeId: string;
+    
+}
+
 class Expense {
     static async getAll({ filters, storeId }: GetAllPropsType, { mongoDbConfig, user }: ConfigType) {
-        return await mongoDbConfig.collections
+        const expenses = await mongoDbConfig.collections
             .WAREHOUSES
             .aggregate([
                 { $match: { id: storeId } },
@@ -43,7 +60,7 @@ class Expense {
                         createdAt: { $first: "$expenses.createdAt" },
                         category: { $first: "$expenses.category" },
                         id: { $first: "$expenses.id" },
-                        items: { $first: "$expenses.items" },
+                        items: { $push: "$expenses.items" },
                         status: { $first: "$expenses.status" },
                         total: { $first: "$expenses.total" },
                         user: { 
@@ -56,7 +73,42 @@ class Expense {
                     }
                 }
             ]).toArray() as ExpenseInfoType[];
+
+        
+        return expenses.sort((expenseA, expenseB) => {
+            return new Date(expenseB.createdAt).getTime() - new Date(expenseA.createdAt).getTime()
+        })
     }
+
+    static async updateStore({ helper, storeId }: UpdateStorePropsType, { mongoDbConfig, user }: ConfigType) {
+        await Store.update<ExpenseType[]>(
+            {
+                helper,
+                id: storeId,
+                key: "expenses"
+            }, 
+            { 
+                mongoDbConfig, 
+                user 
+            }
+        );
+    }
+
+    static async delete({ expenseId, storeId }: DeletePropsType, { mongoDbConfig, user }: ConfigType) {
+        const helper = (store: WarehouseType) => {
+            let expenses = structuredClone(store.expenses);
+
+            const currentExpense = expenses.find(expense => expense.id === expenseId);
+
+            if(!currentExpense) throw new Error404("Expense not found");
+
+            expenses = expenses.filter(expense => expense.id !== expenseId);
+
+            return expenses;
+        };
+
+        await this.updateStore({ helper, storeId }, { mongoDbConfig, user })
+    } 
 
     static async register({ expense, storeId }: PropsType, { mongoDbConfig, user }: ConfigType) {
         const helper = (store: WarehouseType) => {
@@ -83,17 +135,32 @@ class Expense {
             return expenses;
         };
 
-        Store.update<ExpenseType[]>(
-            {
-                helper,
-                id: storeId,
-                key: "expenses"
-            }, 
-            { 
-                mongoDbConfig, 
-                user 
-            }
-        );
+        await this.updateStore({ helper, storeId }, { mongoDbConfig, user });
+    } 
+
+    static async update({ expense, expenseId, storeId }: UpdatePropsType, { mongoDbConfig, user }: ConfigType) {
+        const helper = (store: WarehouseType) => {
+            const expenses = structuredClone(store.expenses);
+
+            const currentExpense = expenses.find(expense => expense.id === expenseId);
+
+            if(!currentExpense) throw new Error404("Expense not found")
+
+            const total = expense.items.reduce((prevValue, currentItem) => {
+                return currency(currentItem.price).add(prevValue).value;
+            }, 0)
+
+            if(expense.total !== total) throw new InvalidArgumentError("Total price is not correct");
+
+            const { category, items } = expense;
+            currentExpense.category = category;
+            currentExpense.items = items;
+            currentExpense.total = total;
+
+            return expenses;
+        };
+
+        await this.updateStore({ helper, storeId }, { mongoDbConfig, user });
     } 
 }
 
