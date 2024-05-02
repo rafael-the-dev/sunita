d27562dafb51bd77717b15a26a8cd095b20b1382
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-02 18:17:05 +0100

    create selected expense route

---
+++ src/app/api/users/[username]/warehouses/[warehouseId]/expenses/[expenseId]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { apiHandler } from "@/middlewares/route-handler";
+
+import ExpenseModel from "@/models/server/db/Expenses";
+import { ExpenseClientType } from "@/types/expense";
+
+type URLParamsType = {
+    params: {
+        expenseId: string;
+        username: string;
+        warehouseId: string;
+    }
+}
+
+export const DELETE = async (req: NextRequest, { params: { expenseId, warehouseId }}: URLParamsType) => {
+
+    return await apiHandler(async ({ mongoDbConfig, user }) => {
+        await ExpenseModel.delete({ expenseId, storeId: warehouseId }, { mongoDbConfig, user });
+        return NextResponse.json("");
+    });
+};
+
+export const GET = async (req: NextRequest, { params: { expenseId, username, warehouseId }}: URLParamsType) => {
+    return await apiHandler(async ({ mongoDbConfig, user }) => {
+        // const products = await ProductModel.getAll({ warehouseId }, { mongoDbConfig, user });
+        // return NextResponse.json(products);
+    });
+};
+
+export const PUT = async (req: NextRequest, { params: { expenseId, warehouseId }}: URLParamsType) => {
+    const expense = await req.json() as ExpenseClientType;
+
+    return await apiHandler(async ({ mongoDbConfig, user }) => {
+        await ExpenseModel.update({ expense, expenseId, storeId: warehouseId }, { mongoDbConfig, user });
+        return NextResponse.json("Expense was successfully updated");
+    });
+};
