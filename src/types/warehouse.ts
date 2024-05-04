e4c83043de0b17731a8264b103d2c091a3e1cba3
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-04 14:49:43 +0100

    add stock-reports field

--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -2,9 +2,11 @@
 import { ExpenseType } from "./expense";
 import { WarehouseProductType } from "./product"
 import { SaleType } from "./sale";
+import { StockReportType } from "./stock";
 
 export type WarehouseType = {
     expenses: ExpenseType[];
     products: WarehouseProductType[];
     sales: SaleType[];
+    "stock-reports": StockReportType[];
 };
