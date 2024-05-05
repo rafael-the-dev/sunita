0b15ace030c8614a67fb0a8bab37851a71700f1d
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-05 21:21:32 +0100

    create new type and new fields

--- src/context/StockContext/types.ts
+++ src/context/StockContext/types.ts
@@ -3,12 +3,27 @@ import { ProductInfoType } from "@/types/product";
 
 export type SetterFuncType = ( product: ProductInfoType, value: number | string, type?: string) => void;
 
+export type StockReportInputProps = {
+    error: boolean;
+    helperText: string;
+    value: string;
+}
+
+export type StockReportType = {
+    date: StockReportInputProps;
+    reference: StockReportInputProps;
+}
+
 export type StockContextType = {
     addItem: (product: ProductInfoType, quantity: number) => void;
     changeQuantity: (id: string, quantity: number) => void;
     getCart: () => CartType<ProductInfoType>;
+    getStockReport: () => StockReportType;
+    hasErrors: () => boolean;
     setQuantity: SetterFuncType;
     setSellPrice: SetterFuncType;
+    setDate: (value: string) => void;
+    setReference: (value: string) => void;
     setTotal: SetterFuncType;
     productsList: ProductInfoType[];
     removeItem: (productId: string) => void;
