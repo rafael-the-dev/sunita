07893f8d39fecacb8e8c118fbe2f6311a0207d7c
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-08 07:17:21 +0100

    create client request stock type

--- src/types/stock.ts
+++ src/types/stock.ts
@@ -1,3 +1,4 @@
+import { ProductInfoType } from "./product";
 
 export type StockClientRequestItemType = {
     product: {
@@ -19,4 +20,31 @@ export type StockClientRequestBodyType = {
 export type StockReportType = StockClientRequestBodyType & {
     id: string;
     modifiedAt: string | Date | null;
+}
+
+export type StockReportInfoItemType = {
+    product: {
+        category: string;
+        barcode?: string;
+        id: string;
+        name: string;
+        purchasePrice: number;
+        sellPrice: number;
+    };
+    quantity: number;
+    total: number;
+}
+
+export type StockReportInfoType = {
+    createdAt: string;
+    id: string;
+    items: StockReportInfoItemType[];
+    modifiedAt: string | null;
+    reference: string;
+    total: number;
+}
+
+export type AnalyticStockReportInfoType = {
+    list: StockReportInfoType[];
+    total: number;
 }
