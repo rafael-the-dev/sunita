5399ee685bdc5a25d18674df3d36cf0f43c559ac
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-09 15:19:23 +0100

    handler logout features to logout button

--- src/components/shared/side-bar/components/logout-button/index.jsx
+++ src/components/shared/side-bar/components/logout-button/index.jsx
@@ -1,16 +1,20 @@
+import { useContext } from "react";
 
 import Icon from "@mui/material/Icon";
 
 import LogoutIcon from '@mui/icons-material/Logout';
 
 import Button from "@/components/shared/button";
+import { LoginContext } from "@/context/LoginContext";
 
 const LogoutButton = () => {
+    const { logout } = useContext(LoginContext)
 
     return (
         <Button 
             color="secondary"
-            className=" text-red-800 w-full">
+            className=" text-red-800 w-full"
+            onClick={logout}>
             <Icon className="flex">
                 <LogoutIcon />
             </Icon>
