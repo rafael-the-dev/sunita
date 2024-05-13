7ba1c81644d8115cbd0b5075241550bcdc7378fc
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-13 10:13:26 +0100

    generalize events

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,5 +1,5 @@
 import { ChangeEvent, MouseEvent } from "react"
 
 
-export type ChangeEventFunc = <T extends HTMLElement>(e: ChangeEvent<T>) => void;
-export type MouseEventFunc = <T extends HTMLElement>(e: MouseEvent<T>) => void;
+export type ChangeEventFunc<T extends HTMLElement> = (e: ChangeEvent<T>) => void;
+export type MouseEventFunc<T extends HTMLElement> = (e: MouseEvent<T>) => void;
