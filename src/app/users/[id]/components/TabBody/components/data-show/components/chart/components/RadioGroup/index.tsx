98995471395b1d67bdb3fbba62d11aff8e1d13d1
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-14 18:27:53 +0100

    customize component to allow checkbox components

--- src/app/users/[id]/components/TabBody/components/data-show/components/chart/components/RadioGroup/index.tsx
+++ src/app/users/[id]/components/TabBody/components/data-show/components/chart/components/RadioGroup/index.tsx
@@ -1,31 +1,34 @@
-
-
+import { MutableRefObject } from "react";
 import FormGroup from "@mui/material/FormGroup";
 
-import Radio from "@/components/radio-button";
-import { MutableRefObject } from "react";
 import { ChangeEventFunc } from "@/types/events";
+
+import Checkbox from "@/components/checkbox"
+import Radio from "@/components/radio-button";
 
 type PropsType = {
     isSelected: (id: string) => boolean;
     list: MutableRefObject<{ label: string, value: string }[]>;
     onChange: ChangeEventFunc<HTMLInputElement>;
+    radio?: boolean;
 }
 
-const Container = ({ isSelected, list, onChange }: PropsType) => {
-
+const Container = ({ isSelected, list, onChange, radio= true }: PropsType) => {
+    
     return (
         <FormGroup row>
             {
-                list.current.map(item => (
-                    <Radio 
-                        checked={isSelected(item.value)} 
-                        key={item.value}
-                        label={ item.label }
-                        onChange={onChange}
-                        value={item.value}
-                    />
-                ))
+                list.current.map(item => {
+                    const itemProps = {
+                        checked: isSelected(item.value),
+                        key: item.value,
+                        label: item.label,
+                        onChange: onChange,
+                        value: item.value
+                    }
+
+                    return radio ? <Radio { ...itemProps } /> : <Checkbox { ...itemProps} />
+                })
             }
         </FormGroup>
     )
