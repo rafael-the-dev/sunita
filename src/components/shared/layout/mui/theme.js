39c8e9d9fccd1faa031d9d0ff70d5edddbfb88ce
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-15 19:38:21 +0100

    add ui breakpoints to material-ui theme

--- src/components/shared/layout/mui/theme.js
+++ src/components/shared/layout/mui/theme.js
@@ -9,6 +9,16 @@ const roboto = Roboto({
 });
 
 export const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 768,
+            lg: 900,
+            xl: 1024,
+            // '2xl': 1280,
+        },
+    },
     typography: {
         fontFamily: roboto.style.fontFamily,
     },
