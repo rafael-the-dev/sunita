05c194dd7a6cb15037cadaf2ece0a4073b43a423
Rafael Tivane <luislangabusiness@gmail.com>
2024-05-16 07:44:27 +0100

    don't close dialog, if AppContext isLoading.current variable is true

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useCallback, useEffect, useMemo, useRef, useState, MutableRefObject } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState, MutableRefObject, useContext } from "react"
 import { usePathname } from "next/navigation"
 import Dialog from "@mui/material/Dialog";
 import DialogAction from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogHeader from "./components/dialog-header";
+import { AppContext } from "@/context/AppContext";
 
 type HandlerRef = MutableRefObject<() => void>;
 
@@ -20,13 +21,19 @@ type DialogProspsType = {
 };
 
 const Container = ({ ariaDescribedby, ariaLabelledby, children, classes, customClose, onCloseRef, onOpenRef }: DialogProspsType) => {
-    const pathname = usePathname();
+    const { isLoading } = useContext(AppContext);
 
+    const pathname = usePathname();
     const currentPath = useRef<string | null>(null);
+
     const [ open, setOpen ] = useState(false);
 
-    const childrenMemo = useMemo(() => children, [ children ])
-    const handleClose = useCallback(() => setOpen(false), []);
+    const childrenMemo = useMemo(() => children, [ children ]);
+
+    const handleClose = useCallback(() => {
+        if(isLoading.current) return;
+        setOpen(false);
+    }, [ isLoading ]);
 
     useEffect(() => {
         onOpenRef.current = () => setOpen(true);
