f2419d3df0edd48fed3dcaa13cf4893bc2faf533
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-11 17:43:22 +0100

    add new field - expenses-category

--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -1,10 +1,11 @@
-
+import { CategoryType as ExpenseCategory } from "./category"
 import { ExpenseType } from "./expense";
 import { WarehouseProductType } from "./product"
 import { SaleType } from "./sale";
 import { StockReportType } from "./stock";
 
 export type WarehouseType = {
+    "expenses-categories": ExpenseCategory[],
     expenses: ExpenseType[];
     products: WarehouseProductType[];
     sales: SaleType[];
