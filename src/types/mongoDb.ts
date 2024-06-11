f05075163c2c36f113379165898d5735abac07b5
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-11 18:02:03 +0100

    add EXPENSES_CATEGORIES field

--- src/types/mongoDb.ts
+++ src/types/mongoDb.ts
@@ -1,9 +1,12 @@
 import { MongoClient, Collection, Db } from "mongodb";
+
+import { CategoryType as ExpenseCategory } from "@/types/category"
 import { UserType } from "./user";
 import { ProductType, GblobalProductType } from "./product";
 import { WarehouseType } from "./warehouse";
 
 export type CollectionType = {
+    EXPENSES_CATEGORIES: Collection<ExpenseCategory>,
     PRODUCTS: Collection<GblobalProductType>;
     USERS: Collection<UserType>;
     WAREHOUSES: Collection<WarehouseType>;
