251702499511804ea332abd02073437875a630b1
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-12 21:06:04 +0100

    filter expenses by category

--- src/app/api/users/[username]/warehouses/[warehouseId]/analytics/expenses/route.ts
+++ src/app/api/users/[username]/warehouses/[warehouseId]/analytics/expenses/route.ts
@@ -2,22 +2,32 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { apiHandler } from "@/middlewares/route-handler";
 import { getExpensesTotalPrice, getFilters } from "@/helpers/analytics";
+
+import { URLParamsType } from "@/types/app-config-server"
 import { AnalyticsExpenseType } from "@/types/analytics";
 
 import Expenses from "@/models/server/db/Expenses";
-
-type URLParamsType = {
-    params: {
-        username: string;
-        warehouseId: string;
-    }
-}
 
 export const GET = async (req: NextRequest, { params: { username, warehouseId }}: URLParamsType) => {
     const searchParams = req.nextUrl.searchParams;
 
     return await apiHandler(req, async ({ mongoDbConfig, user }) => {
-        const expenses = await Expenses.getAll({ filters: getFilters("expenses.createdAt", searchParams), storeId: warehouseId }, { mongoDbConfig, user })
+        const category = searchParams.get("category")
+        
+        const filters = {
+            ...getFilters("expenses.createdAt", searchParams),
+            ...( category ? { "category_info.id": category } : {} )
+        }
+        const expenses = await Expenses.getAll(
+            { 
+                filters, 
+                storeId: warehouseId 
+            }, 
+            { 
+                mongoDbConfig, 
+                user 
+            }
+        )
 
         const analytics:AnalyticsExpenseType = {
             list: expenses,
