be6f660c2a27961ae6a8be302fd500a5f5f9328d
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-12 21:19:18 +0100

    refactor code - use shared expenses-category component

--- src/app/users/[id]/warehouses/[warehouseId]/expenses/components/TabBody/components/add/components/categories/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/expenses/components/TabBody/components/add/components/categories/index.tsx
@@ -1,39 +1,20 @@
-import { ChangeEvent, useCallback, useContext, useState} from "react"
-import classNames from "classnames";
+import { ChangeEvent, useCallback, useContext } from "react"
 
-import styles from "./styles.module.css"
-import { CategoryType, CATEGORY_STATUS } from "@/types/category";
-
-import useFetch from "@/hooks/useFetch";
 import { ExpensesContext } from "@/context/ExpensesContext";
+import { AppContext } from "@/context/AppContext";
 
-import Combobox from "@/components/shared/combobox";
+import ExpensesCategories from "@/components/shared/expenses-categories"
 
 const Categories = () => {
     const { addCategory, getCategory } = useContext(ExpensesContext);
-
-    const { data } = useFetch<CategoryType[]>({
-        autoFetch: true,
-        url: `/api/users/rafaeltivane/warehouses/12345/expenses/categories?status=${CATEGORY_STATUS.ACTIVE}`
-    })
-
-    const getList = () => {
-        if(!data) return [];
-
-        return data.map(({ id, name }) => ({
-            label: name,
-            value: id
-        }))
-    }
+    const { dialog } = useContext(AppContext)
 
     const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => addCategory(e.target.value), [ addCategory ]);
 
     return (
-        <Combobox 
-            className={classNames(styles.input)}
-            label="Category"
-            list={getList()}
+        <ExpensesCategories 
             onChange={changeHandler}
+            showAll={Boolean(dialog.payload)}
             value={getCategory()}
         />
     )
