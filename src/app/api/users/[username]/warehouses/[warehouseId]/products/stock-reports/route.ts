22190d1f8a895ce2043a1a4686cd5d0c90bee96c
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-17 17:40:30 +0100

    filters reports by username

--- src/app/api/users/[username]/warehouses/[warehouseId]/products/stock-reports/route.ts
+++ src/app/api/users/[username]/warehouses/[warehouseId]/products/stock-reports/route.ts
@@ -21,12 +21,13 @@ export const GET = async (req: NextRequest, { params: { productId, warehouseId }
     return await apiHandler(req, async ({ mongoDbConfig, user }) => {
         const endDate = searchParams.get("end-date");
         const products = searchParams.getAll("product");
-        const users = searchParams.getAll("user");
+        const userId = searchParams.get("user");
         const startDate = searchParams.get("start-date");
 
         const filters = {
             ...( resetDate({ endDate, key: "stock-reports.createdAt", startDate }) ),
             ...( products.length > 0 ? { "stock-reports.items": { $elemMatch: { "product.id": { $in: products } } } } : {} ),
+            ...( userId ? { "stock-reports.user": userId } : {} ),
         };
 
         const result = await Stock.getAll(
