6edf61835b3d4d966e6224663ca21677e1062e3c
Rafael Tivane <luislangabusiness@gmail.com>
2024-06-17 17:52:06 +0100

    refactor code and implement stock and product proxies

--- src/models/server/db/Stock.ts
+++ src/models/server/db/Stock.ts
@@ -5,11 +5,16 @@ import { ConfigType } from "@/types/app-config-server";
 import { WarehouseType } from "@/types/warehouse";
 import { WarehouseProductType } from "@/types/product";
 import { AnalyticStockReportInfoType, StockClientRequestBodyType, StockClientRequestItemType, StockReportInfoType, StockReportType } from "@/types/stock";
-import { isValidDate, isValidPrice, isValidReference } from "@/helpers/stock-report";
+import { isValidDate, isValidPrice, isValidReference, updateProduct } from "@/helpers/stock-report";
+
+
 import { getId } from "@/helpers/id";
+import { sort } from "@/helpers/sort";
+import getProductProxy from "../proxy/product";
+import getStockProxy from "../proxy/stock";
 
 import Store from "./Warehouse";
-import { sort } from "@/helpers/sort";
+import Error404 from "@/errors/server/404Error";
 
 
 type AddPropsType = {
@@ -106,123 +111,117 @@ class Stock {
     }
 
     static async add({ storeId, stockDetails }: AddPropsType, { mongoDbConfig, user }: ConfigType) {        
-        const createdAt = moment(stockDetails.createdAt);
-
-        //check if date is not greater than current date, Date.now()
-        isValidDate(createdAt);
-
-        //check if cart's total price is valid
-        isValidPrice(structuredClone(stockDetails))
-
         const reportId = getId();
 
-        let currentProducts: WarehouseProductType[] = [];
-        let currentStockReports: StockReportType[] = [];
-        let hasRetrieveCurrentStoreDetails = false;
+        const stores = await mongoDbConfig
+            .collections
+            .WAREHOUSES
+            .aggregate([
+                { $match: { id: storeId } },
+                { $unwind: "$products" },
+                { $match: { "products.id": { $in: stockDetails.items.map(item => item.product.id )} } },
+                { $group: {
+                    _id: "$_id",
+                    products: {
+                        $push: {
+                            barcode: "$products.barcode",
+                            id: "$products.id",
+                            purchasePrice: "$products.purchasePrice",
+                            sellPrice: "$products.sellPrice",
+                            stock: "$products.stock",
+                        }
+                    },
+                }}
+            ])
+            .toArray() as WarehouseType[]
+        
+        if(!stores || stores?.length === 0) throw new Error404("Store not found");
 
+        let currentProducts: WarehouseProductType[] = [];
+       
         const productsMap = new Map<string, StockClientRequestItemType>()
 
-        const config = {
-            mongoDbConfig,
-            user
-        };
-
         try {
-            const storeDetails = await Store.get({ id: storeId }, { mongoDbConfig, user });
-
-            //throw an InvalidArgumentError if reference is invalid
-            isValidReference(storeDetails, stockDetails.reference);
-            
             stockDetails.items.forEach(item => {
                 productsMap.set(item.product.id, item);
             });
 
-            //clone products and stock-reports upcoming use, in case an error occur
-            currentProducts = structuredClone(storeDetails.products);
-            currentStockReports = structuredClone(storeDetails["stock-reports"]);
+            const report: StockReportType = {
+                createdAt: "",
+                id: reportId,
+                items: [],
+                modifiedAt: null,
+                reference: "",
+                total: 0,
+                user: user.username
+            };
 
-            hasRetrieveCurrentStoreDetails = true;
+            const stockProxy = getStockProxy(report);
 
-            await Promise.all([
-                Store.update<StockReportType[]>(
-                    {
-                        helper(store: WarehouseType) {
-                            const reports = structuredClone(store['stock-reports']);
+            stockProxy.createdAt = stockDetails.createdAt;
+            stockProxy.items = stockDetails.items;
+            stockProxy.reference = stockDetails.reference;
+            stockProxy.total = stockDetails.total;
 
-                            const report: StockReportType = {
-                                createdAt: createdAt.toISOString(),
-                                id: reportId,
-                                items: stockDetails.items,
-                                modifiedAt: null,
-                                reference: stockDetails.reference,
-                                total: stockDetails.total,
-                                user: user.username
-                            }
-
-                            reports.push(report)
-
-                            return reports;
-                        },
-                        id: storeId,
-                        key: "stock-reports"
-                    },
-                    config  
+            await mongoDbConfig
+                .collections
+                .WAREHOUSES
+                .updateOne(
+                    { id: storeId },
+                    { 
+                        $push: {
+                            "stock-reports": report
+                        }
+                    }
                 ),
-                Store.update<WarehouseProductType[]>(
-                    {
-                        helper(store: WarehouseType) {
-                            const products = structuredClone(store.products);
+            
+            await Promise.all(
+                structuredClone(stockDetails).items.map((item) => {
+                    const product = structuredClone(stores[0].products).find(product => product.id === item.product.id)
 
-                            products.forEach(product => {
-                                // get current product from mappedStockItem map
-                                const mappedStockItem = productsMap.get(product.id);
+                    // get current product from mappedStockItem map
+                    const mappedStockItem = productsMap.get(item.product.id);
+                    
+                    if(!product || !mappedStockItem) throw new Error404(`Product with id '${item.product.id}' not found`)
 
-                                /**
-                                 * it was mapped update its quantity, sellPrice, purchasePrice and profit fields base on mapped details
-                                 */
-                                if(mappedStockItem) {
-                                    const mappedStockItemProduct = mappedStockItem.product;
+                    /**
+                     * If It was mapped update its quantity, sellPrice, purchasePrice and profit fields base on mapped details
+                     */
+                    const productProxy = getProductProxy(product)
+                    
+                    const mappedStockItemProduct = mappedStockItem.product;
 
-                                    product.stock.quantity = currency(product.stock.quantity).add(mappedStockItem.quantity).value;
-                                    product.sellPrice = mappedStockItemProduct.sellPrice;
-                                    product.purchasePrice = mappedStockItemProduct.purchasePrice;
-                                    product.profit = currency(mappedStockItemProduct.sellPrice).subtract(mappedStockItemProduct.purchasePrice).value
-                                }
-                            })
-
-                            return products;
-                        },
-                        id: storeId,
-                        key: "products"
-                    },
-                    config 
-                )
-            ])
+                    productProxy.stock.quantity = currency(product.stock.quantity).add(mappedStockItem.quantity).value;
+                    productProxy.sellPrice = mappedStockItemProduct.sellPrice;
+                    productProxy.purchasePrice = mappedStockItemProduct.purchasePrice;
+                    productProxy.profit = currency(mappedStockItemProduct.sellPrice).subtract(mappedStockItemProduct.purchasePrice).value
+                    
+                    return updateProduct(productProxy, storeId, mongoDbConfig)
+                })
+            )
+            
         } catch(e) {
+            console.error(e)
             /**
              * update store with its previous stock-reports and products with occurend an errory
              */
             await Promise.all([
-                Store.update<StockReportType[]>(
-                    {
-                        helper(store: WarehouseType) {
-                            return hasRetrieveCurrentStoreDetails ? currentStockReports : store["stock-reports"];
-                        },
-                        id: storeId,
-                        key: "stock-reports"
-                    },
-                    config  
-                ),
-                Store.update<WarehouseProductType[]>(
-                    {
-                        helper(store: WarehouseType) {
-                            return hasRetrieveCurrentStoreDetails ? currentProducts : store.products;
-                        },
-                        id: storeId,
-                        key: "products"
-                    },
-                    config 
-                )
+                mongoDbConfig
+                    .collections
+                    .WAREHOUSES
+                    .updateOne(
+                        { id: storeId },
+                        { 
+                            $pull: {
+                                "stock-reports": {
+                                    id: reportId
+                                }
+                            }
+                        }
+                    ),
+                ...structuredClone(stores[0].products).map(product => {
+                    return updateProduct(product, storeId, mongoDbConfig)
+                })
             ])
 
             throw e
