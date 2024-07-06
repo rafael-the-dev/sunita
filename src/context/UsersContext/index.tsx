8720c98334a8d688294cd93eb303957962310e8f
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-06 07:45:04 +0100

    create users context for registering and updating new user

---
+++ src/context/UsersContext/index.tsx
@@ -0,0 +1,57 @@
+import { createContext, useCallback, useContext } from "react"
+
+import { ContextType, PropsType } from "./types"
+import { UserType } from "@/types/user"
+
+import { LoginContext } from "../LoginContext"
+
+import useFetch from "@/hooks/useFetch"
+import useSearchParams from "@/hooks/useSearchParams"
+
+export const UsersContext = createContext<ContextType>({} as ContextType)
+
+const isIncluded = (value:string, searchValue: string) => {
+    return value.toLowerCase().includes(searchValue.toLowerCase())
+} 
+
+export const UsersContextProvider = ({ children }: PropsType) => {
+    const { credentials } = useContext(LoginContext)
+
+
+    const { data, loading } = useFetch<UserType[]>({
+        url: `/api/stores/${ credentials?.user?.stores[0]?.storeId }/users`
+    })
+
+    const searchParams = useSearchParams();
+
+    const position = searchParams.get("position", "")
+    const searchParam = searchParams.get("search", "");
+
+
+    const getUsers = useCallback(() => {
+        if(!data) return [];
+
+        if(searchParam.trim()) return data.filter(user => {
+            //value is in...
+            const isInFullName = isIncluded(`${user.firstName} ${user.lastName}`, searchParam);
+            const isInUsername = isIncluded(user.username, searchParam);
+
+            return isInFullName || isInUsername;
+        })
+
+        if(position) return data.filter(user => user.stores[0]?.category === position);
+
+        return data;
+    }, [ data, position, searchParam ]);
+
+    return (
+        <UsersContext.Provider
+            value={{
+                getUsers,
+                loading
+            }}
+            >
+            { children }
+        </UsersContext.Provider>
+    )
+}
---
+++ src/context/UsersContext/types.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+import { UserType } from "@/types/user"
+
+export type PropsType = {
+    children: ReactNode
+}
+
+export type ContextType = {
+    getUsers: () => UserType[],
+    loading: boolean
+}
