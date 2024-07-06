3566c4213b674b1b47f0d709df7a5bd21251aad8
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-06 07:33:50 +0100

    disable delete button if there is one tab in the list, return Button if tab is not active

--- src/components/shared/tabs-list/components/tab/index.tsx
+++ src/components/shared/tabs-list/components/tab/index.tsx
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography"
 import classNames from "classnames"
+
+import CloseIcon from "@mui/icons-material/Close"
 
 import { TabsContext } from "@/context/TabsContext";
 
@@ -10,25 +14,53 @@ type TabProps = {
 }
 
 const TabContainer = ({ id, label }: TabProps) => {
-    const { activateTab, getActiveTab } = useContext(TabsContext)
+    const { activateTab, getActiveTab, getTabsList, removeTab } = useContext(TabsContext)
 
     const isSelected = getActiveTab().id === id;
+    const hasOneTab = getTabsList().length === 1; 
 
     const clickHandler = () => {
-        if(isSelected) return
+        if(isSelected) return;
 
-        activateTab(id)
-    }
+        activateTab(id);
+    };
+
+    const removeHandler = () => removeTab(id);
 
     return (
         <li>
-            <Button 
-                className={classNames(
-                    "rounded-t-none text-black",
-                    { "bg-primary-100 rounded-bl-2xl": isSelected })}
-                onClick={clickHandler}>
-                { label }
-            </Button>
+            { 
+                !isSelected && (
+                    <Button 
+                        className={classNames("rounded-t-none text-black")}
+                        onClick={clickHandler}>
+                        { label }
+                    </Button>
+                ) 
+            }
+            {
+                isSelected && (
+                    <div className={classNames(`flex items-center`,
+                        { "bg-primary-100 pl-2": isSelected },
+                        { "p-2": hasOneTab }
+                    )}>
+                        <Typography
+                            component="span"
+                            className="text-small">
+                            { label }
+                        </Typography>
+                        {
+                            !hasOneTab && (
+                                <IconButton
+                                    className="ml-4 opacity-80 hover:bg-transparent hover:text-red-600"
+                                    onClick={removeHandler}>
+                                    <CloseIcon className="text-[.95rem]" />
+                                </IconButton>
+                            )
+                        }
+                    </div>
+                )
+            }
         </li>
     )
 }
