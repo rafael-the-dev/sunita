d6cb0db37eaf4446ae26bad06afcfa5d98a10946
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-06 07:24:29 +0100

    return DesktopDatePicker based on date prop, and pass error and helperText props to Input component

--- src/components/date/index.tsx
+++ src/components/date/index.tsx
@@ -1,25 +1,41 @@
 import * as React from "react";
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
 
-import { dateTimeFormat } from "@/helpers/date";
+import { dateFormat, dateTimeFormat } from "@/helpers/date";
 
 import Input from "@/components/Textfield";
 
 type ProspType = {
     className?: string;
+    date?: boolean,
+    error?: boolean,
+    helperText?: string,
     label?: string;
+    minDate?: string;
+    maxDate?: string;
     minDateTime?: string;
     maxDateTime?: string;
     onChange: (value: string) => void;
     value: string;
 };
 
-const DateInputFilter = ({ ...rest }: ProspType) => {
+const DateInputFilter = ({ date, error, helperText, ...rest }: ProspType) => {
+    if(date) {
+        return (
+            <DesktopDatePicker 
+                { ...rest }
+                inputFormat={dateFormat}
+                renderInput={ (params) => <Input { ...params } error={error} helperText={helperText} /> }
+            />
+        )
+    }
+
     return (
         <DesktopDateTimePicker
             { ...rest }
             inputFormat={dateTimeFormat}
-            renderInput={(params) => <Input {...params} />}
+            renderInput={(params) => <Input {...params} error={error} helperText={helperText} />}
         />
     );
 } ;
