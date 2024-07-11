9cc8b8d116be0a17ae8508fa231dce7cca1d9ea8
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-11 07:11:39 +0100

    validate price inputs, and pass min and max prices to the input when component is mounted

--- src/components/shared/product-search-box/components/filters/components/price/index.tsx
+++ src/components/shared/product-search-box/components/filters/components/price/index.tsx
@@ -5,27 +5,51 @@ import styles from "./styles.module.css";
 
 import { ProductFilterContext } from "@/context/ProductFilterContext";
 
+import { isInvalidNumber } from "@/helpers/validation";
+
 import Collapse from "../collapse";
 import Input from "@/components/Input";
 
 const Price = () => {
     const { price, setUniqueSearchParams } = React.useContext(ProductFilterContext);
 
-    const changeHandler = React.useCallback((key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUniqueSearchParams(key, e.target.value)
-    }, [ setUniqueSearchParams ]);
+    const minPriceRef = React.useRef<HTMLInputElement | null>(null);
+    const maxPriceRef = React.useRef<HTMLInputElement | null>(null);
+
+    const minPrice = price.min;
+    const maxPrice = price.max;
+
+    const isMinPriceLessThanZero = isInvalidNumber(minPrice, 0);
+
+    const isMaxPriceGreaterThanMinPrice = isInvalidNumber(maxPrice, minPrice);
+
+    const changeHandler = React.useCallback(
+        (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+            setUniqueSearchParams(key, e.target.value)
+        }, 
+        [ setUniqueSearchParams ]
+    );
+
+    React.useEffect(() => {
+        if(minPriceRef.current) minPriceRef.current.value = minPrice.toString();
+    }, [ minPrice ])
+
+    React.useEffect(() => {
+        if(maxPriceRef.current) maxPriceRef.current.value = maxPrice.toString();
+    }, [ maxPrice ])
 
     return (
-        <Collapse title="Price">
+        <Collapse open title="Price">
             <div className="flex items-center justify-between">
                 <div className={classNames(styles.input, "flex flex-col")}>
                     <label htmlFor="min-price">Min</label>
                     <Input 
                         className="!border mt-2 !py-2"
+                        error={isMinPriceLessThanZero}
                         id="min-price"
                         onChange={changeHandler("min-price")}
                         placeholder="Min"
-                        value={ price.min }
+                        ref={minPriceRef}
                     />
                 </div>
                 <span className="mx-[2%] text-2xl">|</span>
@@ -33,10 +57,11 @@ const Price = () => {
                     <label htmlFor="max-price">Max</label>
                     <Input 
                         className="!border mt-2 !py-2"
+                        error={isMaxPriceGreaterThanMinPrice}
                         id="max-price"
                         onChange={changeHandler("max-price")}
                         placeholder="Max"
-                        value={ price.max }
+                        ref={maxPriceRef}
                     />
                 </div>
             </div>
