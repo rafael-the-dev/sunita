045ef821b50247651bcb02f92d2ef7c4ffd94c48
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-11 07:12:52 +0100

    retrieve categories, then pass them to Categories component

--- src/components/shared/product-search-box/components/filters/index.tsx
+++ src/components/shared/product-search-box/components/filters/index.tsx
@@ -4,16 +4,29 @@ import * as React from "react";
 import classNames from "classnames";
 
 import IconButton from "@mui/material/IconButton";
-
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
 import styles from "./styles.module.css";
+import { CategoryType } from "@/types/category";
+
+import { LoginContext } from "@/context/LoginContext";
+
+import useFetch from "@/hooks/useFetch";
 
 import Categories from "./components/categories";
 import Popover from "@/components/popover";
 import Price from "./components/price";
 
 const FiltersContainer = () => {
+    const { credentials } = React.useContext(LoginContext)
+
+    const { data } = useFetch<CategoryType[]>(
+        {
+            url: `/api/stores/${credentials?.user?.stores[0]?.storeId}/products/categories`
+        }
+    )
+
+    const categories = data
 
     const onClickHandlerRef = React.useRef<(e: React.MouseEvent<HTMLButtonElement>) => void | null>(null);
 
@@ -29,9 +42,8 @@ const FiltersContainer = () => {
                 id="Product searcj filters"
                 onClickRef={onClickHandlerRef}>
                 <div className="py-3 px-2">
-                    <Categories />
+                    <Categories list={categories} />
                     <Price />
-                    <Categories />
                 </div>
             </Popover>
         </>
