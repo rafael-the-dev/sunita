f8a393773cd48379a990f4f179cf14ab3b3058b9
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-16 20:16:52 +0100

    validate booking

---
+++ src/validation/booking.ts
@@ -0,0 +1,52 @@
+import moment from "moment"
+
+import { BOOKING_TYPE } from "@/types/room";
+
+type DateType = string | Date | moment.Moment
+
+export const isValidBookingType = (bookingType: BOOKING_TYPE) => {
+    return Object
+        .values(BOOKING_TYPE)
+        .includes(bookingType)
+}
+
+export const validateCheckIn = (checkIn: DateType, checkOut: DateType) => {
+    const checkInDateTime = moment(checkIn)
+    const checkOutDateTime = moment(checkOut)
+
+    if(checkInDateTime.isBefore(moment(Date.now()))) {
+        return {
+            hasError: true,
+            message: 'Check in must not be before current date'
+        }
+    } 
+    else if(checkInDateTime.isAfter(checkOutDateTime)) {
+        return {
+            hasError: true,
+            message: 'Check in must not be after check out date'
+        }
+    }
+
+    return {
+        hasError: false,
+        message: ""
+    }
+}
+
+
+export const validateCheckOut = (checkIn: DateType, checkOut: DateType) => {
+    const checkInDateTime = moment(checkIn)
+    const checkOutDateTime = moment(checkOut)
+
+    if(checkOutDateTime.isBefore(checkInDateTime)) {
+        return {
+            hasError: true,
+            message: 'Check out must not be before check in date'
+        }
+    } 
+
+    return {
+        hasError: false,
+        message: ""
+    }
+}
