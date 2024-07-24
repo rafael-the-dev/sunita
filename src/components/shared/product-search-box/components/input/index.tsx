47e3eeb2501b13041abb79441f243e1094f67e93
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-24 21:39:41 +0100

    use forwardedRef to wrap and pass ref prop to input component

--- src/components/shared/product-search-box/components/input/index.tsx
+++ src/components/shared/product-search-box/components/input/index.tsx
@@ -1,18 +1,21 @@
-
+import { MutableRefObject, forwardRef } from "react"
 
 import { InputProps } from "@/components/Input/type";
 
 import Input from "@/components/Input";
 import classNames from "classnames";
 
-const InputContainer = ({ ...rest }: InputProps) => {
+const InputContainer = forwardRef(({ ...rest }: InputProps, ref: MutableRefObject<HTMLInputElement>) => {
 
     return (
         <Input 
             { ...rest }
             className={classNames("!py-2", rest.className)}
+            ref={ref}
         />
     );
-};
+});
+
+InputContainer.displayName = "InputContainer"
 
 export default InputContainer;
