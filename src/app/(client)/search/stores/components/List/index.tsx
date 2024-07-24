d6031c64b43300dc2756b0d63a806c7855ce6e9a
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-24 21:27:34 +0100

    create List componnt to render stores list

---
+++ src/app/(client)/search/stores/components/List/index.tsx
@@ -0,0 +1,33 @@
+import { useContext } from "react"
+import classNames from "classnames"
+
+import { TABS } from "../Tabs/components/Tab/types"
+
+import { StoresContext } from "../../context"
+
+import useTab from "../Tabs/hooks/useTab"
+
+import StoreCard from "./components/Card"
+
+const List = () => {
+    const { getStores } = useContext(StoresContext)
+
+    const { isActive } = useTab()
+
+    return (
+        <ul className={classNames("flex flex-col gap-y-4", { "hidden md:flex": !isActive(TABS.LIST)})}>
+            {
+                getStores()
+                    .list
+                    .map(store => (
+                        <StoreCard 
+                            { ...store }
+                            key={store.id}
+                        />
+                    ))
+            }
+        </ul>
+    )
+}
+
+export default List
