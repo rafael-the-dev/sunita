a85ba3acdc43586fd0a9aa6953cda6906dca9cba
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-25 22:22:16 +0100

    Render PricingType and Price components, and add flex classes to container tag

--- src/app/(client)/search/stores/components/Filters/index.tsx
+++ src/app/(client)/search/stores/components/Filters/index.tsx
@@ -3,14 +3,18 @@ import classNames from "classnames"
 import styles from "./styles.module.css"
 
 import Amenities from "./components/Amenities"
+import PricingType from "./components/PricingType"
+import Price from "@/components/shared/filters/components/Price"
 import RoomType from "./components/RoomType"
 
 const FiltersContainer = () => {
 
     return (
-        <div className={classNames(styles.container, `overflow-y-auto`)}>
+        <div className={classNames(styles.container, `flex flex-col gap-y-4 items-stretch overflow-y-auto`)}>
             <RoomType />
             <Amenities />
+            <PricingType />
+            <Price className="bg-white" />
         </div>
     )
 }
