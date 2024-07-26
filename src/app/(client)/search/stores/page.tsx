327bef6de9c01f1c3aec15c5dd16efec4420b7b3
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-26 06:12:19 +0100

    Enhancd page UI

--- src/app/(client)/search/stores/page.tsx
+++ src/app/(client)/search/stores/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import { useContext } from "react"
+import classNames from "classnames"
+
+import styles from "./styles.module.css"
 
 import { StoresContext, StoresContextProvider } from "./context"
 
+import Filters from "./components/Filters"
 import List from "./components/List"
 import SearchBox from "./components/Searchbox"
 import Tabs from "./components/Tabs"
@@ -13,11 +17,16 @@ const StoresPage = () => {
 
     return (
         <div>
-            <main className="px-2 pt-6 pb-8 md:px-4">
-                <div>
+            <main className={classNames(styles.container, "gap-x-12 items-stretch px-[5%] pt-8 pb-8 lg:flex")}>
+                <div className={classNames(styles.filtersContainer, `hidden lg:flex`)}>
+                    <Filters />
+                </div>
+                <div className="bg-white grow py-6 px-4">
                     <SearchBox />
                     <Tabs />
-                    <List />
+                    <div className="mt-10 md:flex">
+                        <List />
+                    </div>
                 </div>
             </main>
         </div>
