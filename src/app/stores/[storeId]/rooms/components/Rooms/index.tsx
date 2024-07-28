dc18dd45c9939a16a7915a1b1f3a7822fde482bc
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-28 19:31:02 +0100

    refactor Rooms' component code, Use default View's Room component, and create Filters component

--- src/app/stores/[storeId]/rooms/components/Rooms/index.tsx
+++ src/app/stores/[storeId]/rooms/components/Rooms/index.tsx
@@ -1,77 +1,27 @@
-import { useCallback, useContext, useEffect, useRef } from "react"
+import { useCallback, useContext, useEffect } from "react"
 
-import { LoginContext } from "@/context/LoginContext"
 import { FixedTabsContext } from "@/context/FixedTabsContext"
 import { RoomsContext } from "../../context"
 
 import { RoomType } from "@/types/room"
-import { TableHeadersType } from "@/components/table/types"
 
-import useFetch from "@/hooks/useFetch"
 import useSearchParams from "@/hooks/useSearchParams"
 
 import Button from "@/components/shared/button"
-import RoomForm from "./components/RoomsForm"
-import Status from "@/components/shared/Status"
-import Table from "@/components/shared/table"
+import Filters from "./components/Filters"
 import RoomsForm from "./components/RoomsForm"
+import RoomsTable from "@/views/Booking/components/Rooms"
+import SearchBox from "@/components/shared/Search/Container"
 
 enum DIALOG_TYPE {
     REGISTER_ROOM = "register-room"
 }
 
 const Rooms = () => {
-    const { credentials } = useContext(LoginContext)
     const { setDialog } = useContext(FixedTabsContext)
-    const { fetchRooms, getRooms } = useContext(RoomsContext)
+    const { getRooms } = useContext(RoomsContext)
 
     const searchParams = useSearchParams()
-
-    const tableHeaders = useRef<TableHeadersType[]>(
-        [
-            {
-                label: "Type",
-                key: {
-                    value: "type"
-                }
-            },
-            {
-                label: "Number of rooms",
-                key: {
-                    value: "quantity"
-                }
-            },
-            {
-                label: "Hourly price",
-                key: {
-                    value: "hourlyPrice"
-                }
-            },
-            {
-                label: "Daily price",
-                key: {
-                    value: "dailyPrice"
-                }
-            },
-            {
-                getComponent({ item }) {
-                    const room = item as RoomType
-
-                    return <Status status={room.status}  />
-                },
-                label: "Status",
-                key: {
-                    value: "status"
-                }
-            },
-            
-        ]
-    )
-
-    const getRoomsList = useCallback(
-        () => getRooms(),
-        [ getRooms ]
-    )
 
     const openDialogHandler = useCallback(
         (value: DIALOG_TYPE) => () => searchParams.setSearchParam("dialog", value),
@@ -114,14 +64,17 @@ const Rooms = () => {
 
     return (
         <div className="flex flex-col justify-between h-full">
-            <div>
-                <Table 
-                    data={getRoomsList()}
-                    headers={tableHeaders}
-                    onClickRow={openUpdateRoomDialog}
+            <div className="mb-8">
+                <SearchBox 
+                    className="mb-6"
+                    filters={<Filters />}
+                />
+                <RoomsTable 
+                    data={getRooms()}
+                    onClickTableRow={openUpdateRoomDialog}
                 />
             </div>
-            <div className="flex flex-col gap-y-3 items-stretch mt-8 sm:flex-row sm:gap-x-3 sm:gap-y-0 sm:justify-end">
+            <div className="flex flex-col gap-y-3 items-stretch mt-16 sm:flex-row sm:gap-x-3 sm:gap-y-0 sm:justify-end">
                 <Button
                     className="py-2"
                     onClick={openDialogHandler(DIALOG_TYPE.REGISTER_ROOM)}>
