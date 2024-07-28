c778d3a39c5ea2df37d6cdfc626a857b81b80399
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-28 19:45:52 +0100

    create default Rooms's filters

---
+++ src/views/Booking/components/Rooms/Filters/PricingType/index.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, useCallback } from "react"
+import FormGroup from "@mui/material/FormGroup"
+
+import useSearchParams from "@/hooks/useSearchParams"
+
+import Collapse from "@/components/shared/collapse"
+import RadioButton from "@/components/radio-button"
+
+enum PRICING_TYPES {
+    ALL = "All",
+    DAILY = "daily",
+    HOURLY = "hourly"
+}
+
+const PricingType = () => {
+    const searchParams = useSearchParams()
+
+    const pricingType = searchParams.get("pricing-type", PRICING_TYPES.ALL)
+
+    return (
+        <Collapse classes={{ root: "bg-white" }} title="Type of price">
+            <FormGroup>
+                {
+                    Object
+                        .values(PRICING_TYPES)
+                        .map(value => (
+                            <RadioButton 
+                                checked={searchParams.isChecked(pricingType, value)}
+                                key={value}
+                                label={value}
+                                onChange={searchParams.changeHandler("pricing-type", searchParams.setSearchParam)}
+                                value={value}
+                            />
+                        ))
+                }
+            </FormGroup>
+        </Collapse>
+    )
+}
+
+export default PricingType
---
+++ src/views/Booking/components/Rooms/Filters/RoomType/index.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, useCallback, useMemo } from "react"
+import FormGroup from "@mui/material/FormGroup"
+
+import { ROOM_TYPE } from "@/types/room"
+
+import useSearchParams from "@/hooks/useSearchParams"
+
+import Collapse from "@/components/shared/collapse"
+import RadioButton from "@/components/radio-button"
+
+const RoomType = () => {
+    const searchParams = useSearchParams()
+
+    const room = searchParams.get("room", "")
+
+    const list = useMemo(
+        () => Object
+            .values(ROOM_TYPE),
+        []
+    )
+    return (
+        <Collapse classes={{ root: "bg-white" }} title="Room Type">
+            <FormGroup>
+                {
+                    list.map(item => (
+                        <RadioButton 
+                            checked={searchParams.isChecked(room, item)}
+                            key={item}
+                            label={item}
+                            onChange={searchParams.changeHandler("room", searchParams.setSearchParam)}
+                            value={item}
+                        />
+                    ))
+                }
+            </FormGroup>
+        </Collapse>
+    )
+}
+
+export default RoomType
