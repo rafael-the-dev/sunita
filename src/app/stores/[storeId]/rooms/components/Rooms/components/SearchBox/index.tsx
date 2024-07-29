fb652ed5f9905ed5f2e006f6a20b829c5f5ed8ec
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-29 13:06:46 +0100

    refactor code

---
+++ src/app/stores/[storeId]/rooms/components/Rooms/components/SearchBox/index.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent, useCallback, useEffect, useRef } from "react"
+
+import useSearchParams from "@/hooks/useSearchParams"
+
+import SearchBox from "@/components/shared/Search/Container"
+
+const Container = () => {
+    return (
+        <SearchBox
+            
+        />
+    )
+}
+
+export default Container
