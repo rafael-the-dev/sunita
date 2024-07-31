8184dbe4f3fb7840790423b104d8b5f44a1c5cc7
Rafael Tivane <luislangabusiness@gmail.com>
2024-07-31 14:21:45 +0100

    create custom hook - useContact

---
+++ src/hooks/useContact/index.ts
@@ -0,0 +1,154 @@
+import { ChangeEvent, useCallback, useState } from "react"
+
+import { ContactType, PHONE_TYPE } from "@/types/contact"
+
+import { isValidPhoneType, isValidPhoneNumber } from "@/validation/contact"
+
+type InputType = {
+    error: boolean;
+    helperText: string;
+    value: string;
+}
+
+type InputContactType = {
+    phone: {
+        number: InputType,
+        type: InputType & { value: PHONE_TYPE }
+    }[]
+}
+
+const initialInput = {
+    error: false,
+    helperText: "",
+    value: ""
+}
+
+const initialContact: InputContactType = {
+    phone: [
+        {
+            type: { ...initialInput, value: PHONE_TYPE.WORK },
+            number: structuredClone(initialInput)
+        }
+    ]
+}
+
+const useContact = () => {
+    const [ contact, setContact ] = useState(initialContact)
+
+    const getAvailableTypesList = useCallback(
+        (contact: InputContactType) => {
+            return Object
+                .values(PHONE_TYPE)
+                .filter(
+                    phoneType => !Boolean(
+                        contact
+                            .phone
+                            .find(
+                                phone => phone.type.value === phoneType
+                            )
+                    )
+                )
+                .map(phoneType => ({ label: phoneType, value: phoneType}))
+        },
+        []
+    )
+
+    const getAvailableTypes = useCallback(
+        () => getAvailableTypesList(contact)
+        ,
+        [ contact, getAvailableTypesList ]
+    )
+
+    const getContact = useCallback(
+        () => structuredClone(contact),
+        [ contact ]
+    )
+
+    const addPhoneNumber = useCallback(
+        () => {
+            setContact(
+                contact => {
+                    const list = getAvailableTypesList(contact)
+
+                    if(list.length === 0) return contact;
+
+                    const contactClone = structuredClone(contact)
+
+                    const phoneType = list[0]
+
+                    if(phoneType) {
+                        contactClone
+                            .phone
+                            .push(
+                                {
+                                    type: { ...initialInput, value: phoneType.value },
+                                    number: structuredClone(initialInput)
+                                }
+                            )
+                    }
+
+                    return contactClone
+                }
+            )
+        }, 
+        [ getAvailableTypesList ]
+    )
+
+    const changePhone = useCallback(
+        (id: PHONE_TYPE, prop: "number" | "type") => (e: ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target;
+
+            setContact(
+                contact => {
+                    const contactClone = structuredClone(contact);
+
+                    const phoneItem = contactClone
+                        .phone
+                        .find(item => item.type.value === id);
+
+                    if(!phoneItem) return contact;
+
+                    if(prop === "number") {
+                        const hasError = !isValidPhoneNumber(value);
+
+                        phoneItem.number.value = value;
+                        phoneItem.number.error = hasError;
+                    } else {
+                        const hasError = !isValidPhoneType(value as PHONE_TYPE);
+
+                        phoneItem.type.value = value as PHONE_TYPE;
+                        phoneItem.type.error = hasError;
+                    }
+
+                    return contactClone;
+                }
+            )
+        },
+        [ ]
+    )
+
+    const removePhoneNumber = useCallback(
+        (id: PHONE_TYPE) => () => {
+            setContact(contact => {
+                const contactClone = structuredClone(contact)
+
+                contactClone.phone = contactClone
+                    .phone
+                    .filter(phoneInput => phoneInput.type.value !== id)
+
+                return contactClone
+            })
+        },
+        []
+    )
+
+    return {
+        addPhoneNumber,
+        changePhone,
+        getAvailableTypes,
+        getContact,
+        removePhoneNumber
+    }
+}
+
+export default useContact
