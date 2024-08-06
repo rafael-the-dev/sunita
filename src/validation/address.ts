cd98b3fc995a17b93123c1ca2466556084533e68
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-06 06:46:09 +0100

    change validation logic

--- src/validation/address.ts
+++ src/validation/address.ts
@@ -1,13 +1,16 @@
 
+//The house or building number must start with one digit in the range 1-9, and be follwed by zero or more digits
 export const isValidAddressNumber = (value: string) => {
-    const pattern = /^[1-9]+$/
+    const pattern = /^[1-9][0-9]*$/
 
     return pattern.test(value)
 }
 
 
 export const isValidAddressStreet = (value: string) => {
-    const pattern = /^[a-zA-Z1-9](?:\s[a-zA-Z0-9])+$/
+    /*const pattern = /^[a-zA-Z1-9](?:\s[a-zA-Z0-9])+$/
 
-    return pattern.test(value)
+    return pattern.test(value)*/
+
+    return Boolean(value.trim())
 }
