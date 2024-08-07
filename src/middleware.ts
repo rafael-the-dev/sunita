c8babbfea773313a23d7f37384f123505e5ea647
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-07 07:41:27 +0100

    Fix expired token exception

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken, isAuthenticated } from "./helpers/auth";
 
 export const middleware = async (req: NextRequest) => {
-    const token = getToken(req);
-    const isLoggedIn = await isAuthenticated(token)
-    
-    if(isLoggedIn) return NextResponse.next();
-    else return NextResponse.json("User not authenticated", { status: 401 });
+    try {
+        const token = getToken(req);
+        const isLoggedIn = await isAuthenticated(token)
+        
+        if(isLoggedIn) return NextResponse.next();
+        else return NextResponse.json("User not authenticated", { status: 401 });
+    } catch(e) {
+        return NextResponse.json("User not authenticated", { status: 401 });
+    }
 };
 
 export const config = {
