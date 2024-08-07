fb7920cf83a420aefc0f689316557dfada5eb78f
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-07 21:55:37 +0100

    intall and add svgr/webpack to webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                svgo: false, // optional: disable SVGO optimizations
+              },
+            },
+          ],
+        });
+    
+        return config;
+      },
+};
 
 export default nextConfig;
