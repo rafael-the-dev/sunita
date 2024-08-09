70422a706434d2eb000eeafd500d069418b1fec1
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-09 10:12:56 +0100

    create container for expirable products

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/ExpirableProducts/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/ExpirableProducts/index.tsx
@@ -1,12 +1,38 @@
 
+import classNames from "classnames"
+
+import Collapse from "../Collapse"
 import Container from "../CategoryContainer"
+import DateInput from "@/components/date"
+import Row from "@/components/Form/RegisterUser/components/Row"
+import TextField from "@/components/Textfield"
 
 const ExpirableProductsContainer = () => {
     return (
-        <Container title="Expirable">
-            <div>
-
-            </div>
+        <Container 
+            title="Expirable">
+                <TextField
+                    className={classNames("mb-6 w-full")}
+                    name="barcode-input"
+                    placeholder="Barcode"
+                    label="Barcode"
+                />
+            <Row>
+                <DateInput
+                    className={classNames("mb-0 sm:w-1/2")}
+                    date
+                    label="Manufacture Date"
+                    onChange={() => {}}
+                    value={new Date(Date.now()).toISOString()}
+                />
+                <DateInput
+                    className={classNames("mb-0 sm:w-1/2")}
+                    date
+                    label="Best Before"
+                    onChange={() => {}}
+                    value={new Date(Date.now()).toISOString()}
+                />
+            </Row>
         </Container>
     )
 }
