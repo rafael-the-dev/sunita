6d572b591747e62ce9fcbd76caa55567a32a59f7
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-10 09:48:01 +0100

    create car category fieldset

---
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/Car/index.tsx
@@ -0,0 +1,53 @@
+
+import classNames from "classnames"
+
+import Container from "../CategoryContainer"
+import DateInput from "@/components/date"
+import Label from "../Label"
+import Row from "@/components/Form/RegisterUser/components/Row"
+import TextField from "@/components/Textfield"
+
+const CarProductsContainer = () => {
+
+    return (
+        <Container 
+            className="gap-y-4"
+            title="Car details">
+            <Row>
+                <TextField 
+                    className="mb-0 w-full sm:w-1/2"
+                    label="Model"
+                />
+                <TextField 
+                    className="mb-0 w-full sm:w-1/2"
+                    label="Make"
+                />
+            </Row>
+            <Row>
+                <TextField 
+                    className="mb-0 w-full sm:w-1/2"
+                    label="Transmission"
+                />
+                <TextField 
+                    className="mb-0 w-full sm:w-1/2"
+                    label="Year"
+                />
+            </Row>
+            <div>
+                <Label>Engine</Label>
+                <Row>
+                    <TextField 
+                        className="mb-0 w-full sm:w-1/2"
+                        label="Year"
+                    />
+                    <TextField 
+                        className="mb-0 w-full sm:w-1/2"
+                        label="Year"
+                    />
+                </Row>
+            </div>
+        </Container>
+    )
+}
+
+export default CarProductsContainer
