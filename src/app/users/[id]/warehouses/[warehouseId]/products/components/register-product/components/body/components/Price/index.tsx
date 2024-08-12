896322552069e0c52f44636bc5c3f4ffe9874533
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 06:46:16 +0100

    set value to purchase and sellPrice inputs, and add event handlers

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/Price/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/Price/index.tsx
@@ -1,9 +1,16 @@
+import { useContext } from "react"
+
+import { ProductFormContext } from "../../context"
 
 import Container from "../CategoryContainer"
 import Row from "@/components/Form/RegisterUser/components/Row"
 import TextField from "@/components/Textfield"
 
 const Price = () => {
+    const {
+        changePrice,
+        input
+    } = useContext(ProductFormContext)
 
     return (
         <Container
@@ -11,17 +18,21 @@ const Price = () => {
             title="Price">
             <Row>
                 <TextField
+                    { ...input.price.purchase }
                     className="mb-0 w-full sm:w-1/2"
                     name="purchase-price-input"
                     placeholder="Purchase price"
                     label="Purchase price"
+                    onChange={changePrice("purchase")}
                     required
                 />
                 <TextField
+                    { ...input.price.sell }
                     className="mb-0 w-full sm:w-1/2"
                     name="sell-price-input"
                     placeholder="Sell price"
                     label="Sell price"
+                    onChange={changePrice("sell")}
                     required
                 />
             </Row>
