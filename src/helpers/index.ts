9fe7d5834839c2371a9dece3fd682c37af99f3d2
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 06:36:41 +0100

    create getList helper method

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -9,3 +9,7 @@ export const isPublicRoute = (pathname: string) => {
 
     return isPublicPath
 }
+
+export const getList = (enumList: Object) => Object
+    .values(enumList)
+    .map(item => ({ label: item, value: item }));
