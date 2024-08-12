aeeb75eb70dd686c4ba8e4a38868dcfdee79b603
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 06:44:00 +0100

    set type of InputField as typeof defaultInputField, add car and furniture types and their handlers

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/types.ts
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/types.ts
@@ -2,17 +2,37 @@ import { ChangeEvent, ReactNode } from "react"
 
 import { PRODUCTS_CATEGORIES } from "@/types/product"
 
-type InputField = {
-    error: boolean,
-    helperText: string,
-    value: string
-}
+import { defaultInputField } from "@/config/input"
+
+import { defaultFunicture } from "./hooks/useFurniture"
+import { defaultProduct } from "./hooks/index"
+import { defaultCarInput } from "./hooks/useCar"
+
+type InputField = typeof defaultInputField
+
+type ChangeHandler = (e: ChangeEvent<HTMLInputElement>) => void;
 
 export type ContextType = {
-    changeCategory: (e: ChangeEvent<HTMLInputElement>) => void,
-    input: {
-        category: InputField & { value: PRODUCTS_CATEGORIES }
-    }
+    changeCategory: ChangeHandler,
+    changeColor: ChangeHandler;
+    input: typeof defaultProduct & {
+        car: typeof defaultCarInput,
+        furnicture: typeof defaultFunicture
+    },
+    changeName: ChangeHandler;
+    changePrice: (prop: "purchase" | "sell") => ChangeHandler;
+
+    // CAR
+    changeEngineNumber: ChangeHandler;
+    changeEngineType: ChangeHandler;
+    changeMake: ChangeHandler;
+    changeModel: ChangeHandler;
+    changeTransmission: ChangeHandler;
+    changeYear: ChangeHandler;
+
+    //Furnicture
+    changeDimensions: (prop: "height" | "length" | "width") => ChangeHandler;
+    changeMaterial: ChangeHandler;
 }
 
 export type PropsType = {
