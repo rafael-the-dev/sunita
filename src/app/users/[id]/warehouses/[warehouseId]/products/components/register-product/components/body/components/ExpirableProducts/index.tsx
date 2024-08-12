aecadf31343232b51fabd243167446a0902a7f92
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 08:31:19 +0100

    add event handlers

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/ExpirableProducts/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/ExpirableProducts/index.tsx
@@ -1,5 +1,10 @@
-
+import { useContext } from "react"
 import classNames from "classnames"
+import moment from "moment"
+
+import { dateFormat } from "@/helpers/date"
+
+import { ProductFormContext } from "../../context"
 
 import Container from "../CategoryContainer"
 import DateInput from "@/components/date"
@@ -7,29 +12,39 @@ import Row from "@/components/Form/RegisterUser/components/Row"
 import TextField from "@/components/Textfield"
 
 const ExpirableProductsContainer = () => {
+    const {
+        changeBarcode,
+        changeExpirationDate,
+        changeManufactureDate,
+        input
+    } = useContext(ProductFormContext)
+
     return (
         <Container 
             title="Expirable">
                 <TextField
+                    { ...input.expirable.barcode }
                     className={classNames("mb-6 w-full")}
-                    name="barcode-input"
                     placeholder="Barcode"
                     label="Barcode"
+                    onChange={changeBarcode}
                 />
             <Row>
                 <DateInput
+                    { ...input.expirable.manufactureDate }
                     className={classNames("mb-0 sm:w-1/2")}
                     date
                     label="Manufacture Date"
-                    onChange={() => {}}
-                    value={new Date(Date.now()).toISOString()}
+                    maxDate={moment(moment.now()).toISOString()}
+                    onChange={changeManufactureDate}
                 />
                 <DateInput
+                    { ...input.expirable.expirationDate }
                     className={classNames("mb-0 sm:w-1/2")}
                     date
                     label="Best Before"
-                    onChange={() => {}}
-                    value={new Date(Date.now()).toISOString()}
+                    minDate={input.expirable.manufactureDate.value}
+                    onChange={changeExpirationDate}
                 />
             </Row>
         </Container>
