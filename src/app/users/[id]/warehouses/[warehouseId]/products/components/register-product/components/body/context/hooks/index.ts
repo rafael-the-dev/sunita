ed2ae3fa339c3dc4b2aa488eeb2e7d14e7e50c62
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 08:29:08 +0100

    add description field and its event handler, create set purchase and sell price functions and validate each field when other changes

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/hooks/index.ts
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/hooks/index.ts
@@ -10,6 +10,7 @@ import { isValidCategory, isValidColor, isValidName, isValidPurchasePrice, isVal
 export const defaultProduct = {
     category: { ...defaultInputField, value: PRODUCTS_CATEGORIES.EXPIRABLE },
     color: structuredClone(defaultInputField ),
+    description: structuredClone(defaultInputField ),
     name: structuredClone(defaultInputField ),
     price: {
         purchase: structuredClone(defaultInputField),
@@ -17,11 +18,28 @@ export const defaultProduct = {
     }
 }
 
+const setPurchasePrice = (value: string, product: typeof defaultProduct) => {
+    const hasError = !isValidPurchasePrice(value, currency(product.price.sell.value).value)
+
+    product.price.purchase.error = hasError
+    product.price.purchase.helperText = hasError ? "Invalid price or It is greater than sell price" : "",
+    product.price.purchase.value = value
+}
+
+const setSellPrice = (value: string, product: typeof defaultProduct) => {
+    const hasError = !isValidSellPrice(value, currency(product.price.purchase.value).value)
+
+    product.price.sell.error = hasError
+    product.price.sell.helperText = hasError ? "Invalid price or It less than purchase price" : "",
+    product.price.sell.value = value
+}
+
+
 export const useProduct = () => {
     const [ product, setProduct ] = React.useState(defaultProduct)
 
     const changeHelper = React.useCallback(
-        (prop: "category" | "color" | "name", value: string, fn: (value: string) => boolean) => {
+        (prop: "category" | "color" | "description" | "name", value: string, fn: (value: string) => boolean) => {
             const hasError = !fn(value)
 
             setProduct(
@@ -50,6 +68,11 @@ export const useProduct = () => {
         [ changeHelper ]
     )
 
+    const changeDescription = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => changeHelper("description", e.target.value, (value: string) => Boolean(value.trim())),
+        [ changeHelper ]
+    )
+
 
     const changeName = React.useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => changeHelper("name", e.target.value, isValidName),
@@ -62,25 +85,17 @@ export const useProduct = () => {
 
             setProduct(
                 product => {
-                    let hasError = false
+                    const productClone = structuredClone(product)
 
                     if(prop === "purchase") {
-                        hasError = !isValidPurchasePrice(value, currency(product.price.sell.value).value)
+                        setPurchasePrice(value, productClone)
+                        setSellPrice(productClone.price.sell.value, productClone)
                     } else {
-                        hasError = !isValidSellPrice(value, currency(product.price.purchase.value).value)
+                        setSellPrice(value, productClone)
+                        setPurchasePrice(productClone.price.purchase.value, productClone)
                     }   
                     
-                    return {
-                        ...product,
-                        price: {
-                            ...product.price,
-                            [prop]: {
-                                error: hasError,
-                                helperText: hasError ? "Invalid price" : "",
-                                value
-                            }
-                        }
-                    }
+                    return productClone
                 }
             )
         },
@@ -91,6 +106,7 @@ export const useProduct = () => {
         product,
         changeCategory,
         changeColor,
+        changeDescription,
         changeName,
         changePrice
     }
