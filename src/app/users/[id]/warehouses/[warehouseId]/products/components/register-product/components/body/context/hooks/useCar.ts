50374cdec138ada2f554857137e81415239fe934
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 06:39:45 +0100

    add color, make, model, transmission, and year fields, then create their change handlers

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/hooks/useCar.ts
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/hooks/useCar.ts
@@ -1,18 +1,26 @@
 import { ChangeEvent, useCallback, useState } from "react"
 
-import { CAR_ENGINE_TYPE } from "@/types/product"
+import { CAR_ENGINE_TYPE, CAR_TRANSMISSION } from "@/types/product"
 
 import { defaultInputField } from "@/config/input"
 
 import { 
-    isValidEngineNumber, isValidEngineType 
+    isValidEngineNumber, isValidEngineType,
+    isValidMake, isValidModel,
+    isValidTransmission,
+    isValidYear
 } from "@/validation/product/car"
 
 export const defaultCarInput = {
+    color: structuredClone(defaultInputField),
     engine: {
         number: structuredClone(defaultInputField),
         type: { ...defaultInputField, value: CAR_ENGINE_TYPE.DIESEL }
-    }
+    },
+    make: structuredClone(defaultInputField),
+    model: structuredClone(defaultInputField),
+    transmission: { ...defaultInputField, value: CAR_TRANSMISSION.AUTOMATIC },
+    year: structuredClone(defaultInputField),
 }
 
 export const useCar = () => {
@@ -25,16 +33,15 @@ export const useCar = () => {
 
             setCar(
                 car => ({
-                        ...car,
-                        engine: {
-                            ...car.engine,
-                            [prop]: {
-                                error: hasError,
-                                helperText: hasError ? "Invalid number" : "",
-                                value
-                            }
+                    ...car,
+                    engine: {
+                        ...car.engine,
+                        [prop]: {
+                            error: hasError,
+                            helperText: hasError ? "Invalid value" : "",
+                            value
                         }
-                    }
+                    }}
                 )
             )
 
@@ -42,27 +49,62 @@ export const useCar = () => {
         []
     )
 
-    const changeEngineNumber = useCallback(
-        (e: ChangeEvent<HTMLInputElement>) => {
-            const { value } = e.target
+    const changeHandler = useCallback(
+        (prop: "make" | "model" | "transmission" | "year", value: string, fn: (value: string) => boolean) => {
+            const hasError = !fn(value)
 
-            changeEngine("number", value, isValidEngineNumber)
+            setCar(
+                car => ({
+                    ...car,
+                        [prop]: {
+                            error: hasError,
+                            helperText: hasError ? "Invalid value" : "",
+                            value
+                        }
+                    }
+                )
+            )
         },
+        []
+    )
+
+    const changeEngineNumber = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => changeEngine("number", e.target.value, isValidEngineNumber),
         [ changeEngine ]
     )
 
     const changeEngineType = useCallback(
-        (e: ChangeEvent<HTMLInputElement>) => {
-            const { value } = e.target
-
-            changeEngine("type", value, isValidEngineType)
-        },
+        (e: ChangeEvent<HTMLInputElement>) => changeEngine("type", e.target.value, isValidEngineType),
         [ changeEngine ]
+    )
+
+    const changeModel = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => changeHandler("model", e.target.value, isValidModel),
+        [ changeHandler ]
+    )
+
+    const changeMake = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => changeHandler("make", e.target.value, isValidMake),
+        [ changeHandler ]
+    )
+
+    const changeTransmission = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => changeHandler("transmission", e.target.value, isValidTransmission),
+        [ changeHandler ]
+    )
+    
+    const changeYear = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => changeHandler("year", e.target.value, isValidYear),
+        [ changeHandler ]
     )
 
     return {
         car,
         changeEngineNumber,
-        changeEngineType
+        changeEngineType,
+        changeMake, 
+        changeModel,
+        changeTransmission,
+        changeYear
     }
 }
