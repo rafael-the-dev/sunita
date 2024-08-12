35a0b3c0b3312b561982220361392d1a21cad65c
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-12 06:47:07 +0100

    add values and onChange event handlers

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/Furniture/index.tsx
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/components/Furniture/index.tsx
@@ -1,5 +1,7 @@
-
+import { useContext } from "react"
 import classNames from "classnames"
+
+import { ProductFormContext } from "../../context"
 
 import Container from "../CategoryContainer"
 import Label from "../Label"
@@ -7,29 +9,42 @@ import Row from "@/components/Form/RegisterUser/components/Row"
 import TextField from "@/components/Textfield"
 
 const FurnitureDetailsContainer = () => {
+    const { 
+        input,
+        changeDimensions,
+        changeMaterial
+    } = useContext(ProductFormContext)
 
     return (
         <Container 
             className="gap-y-4"
             title="Furniture details">
             <TextField 
+                { ...input.furnicture.material }
                 className="mb-0 w-full"
                 label="Material"
+                onChange={changeMaterial}
             />
             <div>
                 <Label>Dimensions</Label>
                 <Row>
                     <TextField 
-                        className="mb-0 w-full sm:w-1/3"
-                        label="Height"
-                    />
-                    <TextField 
+                        { ...input.furnicture.dimensions.length }
                         className="mb-0 w-full sm:w-1/3"
                         label="Length"
+                        onChange={changeDimensions("length")}
                     />
                     <TextField 
+                        { ...input.furnicture.dimensions.width }
                         className="mb-0 w-full sm:w-1/3"
                         label="Width"
+                        onChange={changeDimensions("width")}
+                    />
+                    <TextField 
+                        { ...input.furnicture.dimensions.height }
+                        className="mb-0 w-full sm:w-1/3"
+                        label="Height"
+                        onChange={changeDimensions("height")}
                     />
                 </Row>
             </div>
