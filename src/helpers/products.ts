ea7102a94a951cf1f975ec5beb77fc35088db15a
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-13 18:11:30 +0100

    [refactor] remove store and product table aggregation, because product details are store in a single Product table

--- src/helpers/products.ts
+++ src/helpers/products.ts
@@ -4,38 +4,15 @@ import { MongoDbConfigType } from "@/types/mongoDb";
 import { ProductInfoType, WarehouseProductType } from "@/types/product";
 
 export const getProducts = async ({ filter }: { filter: Object }, { mongoDbConfig }: ConfigType) => {
-    const products = await mongoDbConfig.collections.WAREHOUSES.aggregate([
-        // Match the store by its ID
-        { $unwind: '$products' },
-        { $match: filter },
-        // Lookup products that belong to the store
-        {
-            $lookup: {
-                from: 'products', // The name of the Product collection
-                localField: 'products.id',
-                foreignField: 'id',
-                as: 'productsInfo'
-            }
-        },
-        // Unwind the products array to get individual product documents
-        { $unwind: '$productsInfo' },
-        // Project to reshape the output and remove unnecessary fields
-        {
-            $project: {
-                barcode: '$productsInfo.barcode',
-                category: '$productsInfo.category',
-                id: '$productsInfo.id',
-                name: '$productsInfo.name',
-                purchasePrice: '$products.purchasePrice',
-                profit: '$products.profit',
-                sellPrice: '$products.sellPrice',
-                stock: '$products.stock'
-                
-                // Include additional fields as needed
-            }
-        }
-    ])
-    .toArray() as ProductInfoType[];
+    const products = await mongoDbConfig
+        .collections
+        .PRODUCTS
+        .aggregate(
+            [
+                { $match: filter }
+            ]
+        )
+        .toArray() as ProductInfoType[];
     
     return products;
 }
