25b77f22dc353a21d9e4f1454f6cd2b10bc8ddfa
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-13 18:14:04 +0100

    [refactor] validate and remove unnecessary code, save Product data in the Product table, and change getAll products' filter

--- src/models/server/db/Product.ts
+++ src/models/server/db/Product.ts
@@ -1,10 +1,13 @@
 import { v4 as uuidV4 } from "uuid";
 import currency from "currency.js";
 
-import { GblobalProductType, ProductType, WarehouseProductType } from "@/types/product";
+import { GblobalProductType, PRODUCT_STATUS, ProductType, StoreProductType, WarehouseProductType } from "@/types/product";
 import { ConfigType } from "@/types/app-config-server";
 
+import { toISOString } from "@/helpers/date"
 import { getProducts } from "@/helpers/products"
+
+import getProductProxy from "../proxy/product";
 
 import Error404 from "@/errors/server/404Error";
 
@@ -17,9 +20,18 @@ class Product {
         return products
     }
 
-    static async getAll({ filters, storeId }: { filters?: Object, storeId: string }, { mongoDbConfig, user }: ConfigType) {
+    static async getAll({ filters, storeId }: { filters?: Object, storeId: string }, config: ConfigType) {
         try {
-            const products = await getProducts({ filter: { ...filters, id: storeId } }, { mongoDbConfig, user })
+            const products = await getProducts(
+                { 
+                    filter: { 
+                        ...filters, 
+                        stores: storeId 
+                    } 
+                }, 
+                config
+            )
+
             return products;
         } catch (error) {
               console.error('Error retrieving products:', error);
@@ -34,7 +46,7 @@ class Product {
                     .collections
                     .PRODUCTS
                     .deleteOne({ id }),
-                mongoDbConfig
+                /*mongoDbConfig
                     .collections
                     .WAREHOUSES
                     .updateOne(
@@ -46,51 +58,67 @@ class Product {
                                 } 
                             }
                         }
-                    )
+                    )*/
             ]
         )
     }
 
-    static async register({ product, storeId }: { product: ProductType, storeId: string }, { mongoDbConfig, user }: ConfigType) {
-        const { barcode, category, name, purchasePrice, sellPrice } = product
+    static async register({ product, storeId }: { product: StoreProductType, storeId: string }, { mongoDbConfig, user }: ConfigType) {
+        const { 
+            car,
+            category, 
+            cloth,
+            expirable,
+            furnicture,
+            name, 
+            purchasePrice, 
+            sellPrice 
+        } = product
         const productId = uuidV4();
 
         try {
-            const product: GblobalProductType = {
-                barcode,
-                category,
+            const storeProduct: StoreProductType = {
+                createdAt: toISOString(new Date(Date.now())),
+                category: null,
                 id: productId,
-                name,
-                warehouses: [ storeId ]
-            };
-    
-            const warehouseProduct: WarehouseProductType = {
-                id: productId,
-                purchasePrice,
+                name: null,
+                purchasePrice: null,
                 profit: currency(sellPrice).subtract(purchasePrice).value,
-                stock: {
-                    quantity: 0
-                },
-                sellPrice
+                status: PRODUCT_STATUS.ACTIVE,
+                stock: { quantity: 0 },
+                sellPrice,
+                username: user.username,
+                stores: [ storeId ]
             };
-    
-            await mongoDbConfig.collections
-                .PRODUCTS
-                .insertOne(product);
 
+            const productProxy = getProductProxy(storeProduct)
+
+            productProxy.car = car;
+            productProxy.category = category;
+            productProxy.expirable = expirable;
+            productProxy.furnicture = furnicture;
+            productProxy.name = name;
+            productProxy.purchasePrice = purchasePrice;
+            productProxy.sellPrice = sellPrice;
+    
             await mongoDbConfig
                 .collections
-                .WAREHOUSES
+                .PRODUCTS
+                .insertOne(storeProduct);
+
+            /*await mongoDbConfig
+                .collections
+                .WAREHOUSES 
                 .updateOne(
                     {
                         id: storeId
                     },
                     {
                         $push: {
-                            products: warehouseProduct
+                            products: storeProduct
                         }
                     }
-                )
+                )*/
         } catch(e) {
             //delete this product if occured an error
             await this.delete({ id: productId, storeId }, { mongoDbConfig, user })
