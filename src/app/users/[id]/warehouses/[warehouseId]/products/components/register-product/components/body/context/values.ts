778f483dafc3137cfdc6a5624f1d26f7b04f04d7
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-13 21:16:48 +0100

    create functions to return initial values of car, furnicture, expirable product

--- src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/values.ts
+++ src/app/users/[id]/warehouses/[warehouseId]/products/components/register-product/components/body/context/values.ts
@@ -1,50 +1,64 @@
 
-import { CAR_ENGINE_TYPE, CAR_TRANSMISSION, PRODUCTS_CATEGORIES } from "@/types/product"
+import { CAR_ENGINE_TYPE, CAR_TRANSMISSION, PRODUCTS_CATEGORIES, ProductInfoType } from "@/types/product"
 
 import { defaultInputField } from "@/config/input"
 
-export const defaultCar = {
-    color: structuredClone(defaultInputField),
-    engine: {
-        number: structuredClone(defaultInputField),
-        type: { ...defaultInputField, value: CAR_ENGINE_TYPE.DIESEL }
-    },
-    make: structuredClone(defaultInputField),
-    model: structuredClone(defaultInputField),
-    transmission: { ...defaultInputField, value: CAR_TRANSMISSION.AUTOMATIC },
-    year: structuredClone(defaultInputField),
-}
+const getInitialValues = (value?: string) => structuredClone({ ...defaultInputField, value: value ?? "" })
 
-export const defaultExpirableProduct = {
-    barcode: structuredClone(defaultInputField),
-    expirationDate: structuredClone(defaultInputField),
-    manufactureDate: structuredClone(defaultInputField)
-}  
-
-export const defaultFurnicture = {
-    material: { ...defaultInputField },
-    dimensions: {
-        height: structuredClone(defaultInputField),
-        length: structuredClone(defaultInputField),
-        width: structuredClone(defaultInputField)
+export const getDefaultCar = (product?: ProductInfoType) => (
+    {
+        color: getInitialValues(product?.car?.color),
+        engine: {
+            number: getInitialValues(product?.car?.engine?.horsepower?.toString()),
+            type: { ...defaultInputField, value: product?.car?.engine?.type ?? CAR_ENGINE_TYPE.DIESEL }
+        },
+        make: getInitialValues(product?.car?.make),
+        model: getInitialValues(product?.car?.model),
+        transmission: { ...defaultInputField, value: product?.car?.transmission ?? CAR_TRANSMISSION.AUTOMATIC },
+        year: getInitialValues(product?.car?.year?.toString()),
     }
-}
+)
 
-export const defaultProduct = {
-    category: { ...defaultInputField, value: PRODUCTS_CATEGORIES.EXPIRABLE },
-    color: structuredClone(defaultInputField ),
-    description: structuredClone(defaultInputField ),
-    name: structuredClone(defaultInputField ),
-    price: {
-        purchase: structuredClone(defaultInputField),
-        sell: structuredClone(defaultInputField)
+export const getDefaultExpirableProduct = (product?: ProductInfoType) => (
+    {
+        barcode: getInitialValues(product?.expirable?.barcode),
+        expirationDate: getInitialValues(product?.expirable?.expirationDate),
+        manufactureDate: getInitialValues(product?.expirable?.manufactureDate)
+    }  
+)
+
+export const getFurnicture = (product?: ProductInfoType) => (
+    {
+        material: getInitialValues(product?.furnicture?.material),
+        dimensions: {
+            height: getInitialValues(product?.furnicture?.dimensions?.height?.toString()),
+            length: getInitialValues(product?.furnicture?.dimensions?.length?.toString()),
+            width: getInitialValues(product?.furnicture?.dimensions?.width?.toString())
+        }
     }
-}
+)
+
+export const getDefaultProduct = (product?: ProductInfoType) => (
+    {
+        category: { ...defaultInputField, value: product?.category ?? PRODUCTS_CATEGORIES.EXPIRABLE },
+        color: structuredClone(defaultInputField ),
+        description: getInitialValues(""),
+        name: getInitialValues(product?.name),
+        price: {
+            purchase: getInitialValues(product?.purchasePrice?.toString()),
+            sell: getInitialValues(product?.sellPrice?.toString())
+        }
+    }
+)
+
+export const getDefaultProductValues = (product?: ProductInfoType) => (
+    {
+        ...getDefaultProduct(product),
+        car: getDefaultCar(product),
+        expirable: getDefaultExpirableProduct(product),
+        furnicture: getFurnicture(product)
+    }
+)
 
 
-export const defaultInput = {
-    ...defaultProduct,
-    car: defaultCar,
-    expirable: defaultExpirableProduct,
-    furnicture: defaultFurnicture
-}
+export const defaultInput = getDefaultProductValues()
