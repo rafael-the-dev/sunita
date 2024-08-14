05be4fc2ba75b991f2a00743b6e56e6bde7c925f
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-14 08:24:23 +0100

    create PUT method handler

--- src/app/api/stores/[storeId]/products/[productId]/route.ts
+++ src/app/api/stores/[storeId]/products/[productId]/route.ts
@@ -5,6 +5,7 @@ import Product from "@/models/server/db/Product";
 import { URLParamsType } from "@/types/app-config-server";
 
 import { apiHandler } from "@/middlewares/route-handler";
+import { ProductInfoType } from "@/types/product";
 
 export const DELETE = async (req: NextRequest, { params: { productId, storeId } }: URLParamsType) => {
     return await apiHandler(req, async ({ mongoDbConfig, user }) => {
@@ -19,5 +20,18 @@ export const GET = async (req: NextRequest, { params: { productId, storeId } }: 
         const product = await Product.get({ productId, storeId }, { mongoDbConfig, user });
 
         return NextResponse.json(product);
+    });
+};
+
+export const PUT = async (req: NextRequest, { params: { productId, storeId } }: URLParamsType) => {
+    return await apiHandler(req, async ({ mongoDbConfig, user }) => {
+        const product = await req.json() as ProductInfoType
+        product.id = productId
+
+        //if(product.id !== productId) return NextResponse.json({ message: "Product ID and product query param does not match" }, { status: 400 })
+
+        await Product.update({ product, storeId }, { mongoDbConfig, user });
+
+        return NextResponse.json({ message: "Product was successfully registered" });
     });
 };
