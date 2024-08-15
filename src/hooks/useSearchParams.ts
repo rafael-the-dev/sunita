54ac222b345bf0484515cdb36e5b818735ba1919
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-15 06:20:32 +0100

    [refactor] remove unnecessary code and use URLSearchParams API

--- src/hooks/useSearchParams.ts
+++ src/hooks/useSearchParams.ts
@@ -4,12 +4,23 @@ import * as React from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const useSearchParamsHook = () => {
-    const params = useSearchParams();
+    const searchParams = useSearchParams();
     const pathname = usePathname();
     const router = useRouter();
 
+    const createQueryString = React.useCallback(
+        (name: string, value: string) => {
+          const params = new URLSearchParams(searchParams.toString())
+          params.set(name, value)
+     
+          return params.toString();
+        },
+        [ searchParams ]
+    )
+
     const get =  React.useCallback((key: string, defaultValue: any) => {
-        const value = params.get(key);
+        const params = new URLSearchParams(searchParams.toString())
+        const value = params.get(key)
 
         if(value) {
             try {
@@ -20,74 +31,52 @@ const useSearchParamsHook = () => {
         }
 
         return defaultValue;
-    }, [ params ]);
+    }, [ searchParams ]);
+
+    const set = React.useCallback(
+        (key: string, value: string) => {
+            router.push(`${pathname}?${createQueryString(key, value)}`);
+        }, 
+        [ createQueryString, pathname, router ]
+    )
     
-    const getAll = React.useCallback((key: string) => params.getAll(key).map(value => value.replaceAll("--", " ")), [ params ]);
+    const getAll = React.useCallback(
+        (key: string) => {
+            const params = new URLSearchParams(searchParams.toString())
+
+            const list = params
+                .getAll(key)
+                .map(value => value.replaceAll("--", " "))
+
+            return list
+        }, 
+        [ searchParams ]
+    );
 
     const isChecked = React.useCallback((origin: string | string[], value: string): boolean => origin.includes(value), []);
 
     const removeSearchParam = React.useCallback((key: string) => {
-        let searchParams = "";
+        const params = new URLSearchParams(searchParams.toString())
+        params.delete(key)
 
-        params.forEach((value, currentKey) => {
-            if(currentKey === key) searchParams += ``;
-            else searchParams += `${currentKey}=${value}&`;
-        });
+        router.push(`${pathname}?${params.toString()}`);
+    }, [ pathname, router, searchParams ])
 
-        router.push(`${pathname}?${searchParams}`);
-    }, [ params, pathname, router ])
+    const getSearchParams = React.useCallback(
+        (key: string, isUnique: boolean = false) => getAll(key), [ getAll ]);
 
-    const getSearchParams = React.useCallback((key: string, isUnique: boolean = false) => {
-        let searchParams = "";
+    const setSearchParams = React.useCallback(
+        (key: string, value: string) =>  set(key, value), 
+        [ set ]
+    );
 
-        params.forEach((value, currentKey) => {
-            if(isUnique) {
-                if(currentKey === key) searchParams += ``;
-                else searchParams += `${currentKey}=${value}&`;
-            } else {
-                searchParams += `${currentKey}=${value}&`
-            }
-        });
-
-        return searchParams;
-    }, [ params ]);
-
-    const setSearchParams = React.useCallback((key: string, value: string) => {
-        const searchParam = params.getAll(key);
-        let currentSearchParams = "";
-        let searchParams = ``;
-
-        if(searchParam.length > 0) {
-            // get current search params different of selected key
-            params.forEach((value, currentKey) => {
-                if(currentKey !== key) searchParams += `${currentKey}=${value}&`
-            });
-
-            if(searchParam.includes(value)) {
-                // remove current search param from search params list
-                searchParams += searchParam
-                    .filter(paramValue => paramValue !== value)
-                    .map((paramValue => `${key}=${paramValue}&`))
-                    .join("");
-            } else {
-                // add current value to search params' key
-                searchParams += searchParam.map((paramValue => `${key}=${paramValue}&`)).join("");
-                searchParams += `${key}=${value}`;
-            }
-        } else {
-            currentSearchParams = getSearchParams(key);
-            searchParams = `${currentSearchParams}${key}=${value}`;
-        }
-
-        router.push(`${pathname}?${searchParams}`);
-    }, [ getSearchParams, params, pathname, router]);
-
-    const setSearchParam = React.useCallback((key: string, value: string) => {
-        let searchParams = `${getSearchParams(key, true)}${key}=${value.replaceAll(" ", "--")}`;
-
-        router.push(`${pathname}?${searchParams}`);
-
-    }, [ getSearchParams, pathname, router ]);
+    const setSearchParam = React.useCallback(
+        (key: string, value: string) => {
+            removeSearchParam("key")
+            set(key, value)
+        }, 
+        [ removeSearchParam, set ]
+    );
 
     const changeHandler = React.useCallback(
         (key: string, func: (key: string, value: string) => void) => (e: React.ChangeEvent<HTMLInputElement>) => func(key, e.target.value),
