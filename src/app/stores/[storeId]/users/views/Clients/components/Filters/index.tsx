9dcb93f7fdc2ca4e8d7866aa93766e3e023d4cd0
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-19 13:25:44 +0100

    Create filters component

---
+++ src/app/stores/[storeId]/users/views/Clients/components/Filters/index.tsx
@@ -0,0 +1,12 @@
+
+import Searchbox from "@/components/shared/Search/Container"
+
+const FiltersContainer = () => {
+
+    return (
+        <Searchbox 
+        />
+    )
+}
+
+export default FiltersContainer
