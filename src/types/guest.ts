4c01e47f80d324dd43089ca6ba82152c970bf0f8
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-19 13:29:15 +0100

    Create new type - CustomerType

--- src/types/guest.ts
+++ src/types/guest.ts
@@ -1,4 +1,12 @@
+import { ContactType } from "./contact";
 import { Document } from "./user";
+
+export type CustomerType = {
+    contact: ContactType;
+    document: Document,
+    firstName: string;
+    lastName: string;
+}
 
 export type GuestType = {
     document: Document,
