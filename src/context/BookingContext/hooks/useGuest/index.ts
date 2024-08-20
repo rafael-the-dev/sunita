9945513f915ef0c03e5c5b9ecf300d05a3082f89
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-20 22:11:26 +0100

    replace initialInput by defaultInputField, add useContact hook, replace document fields test by document.hasErrors method, add contact reset to rest method, and create to literal object method

--- src/context/BookingContext/hooks/useGuest/index.ts
+++ src/context/BookingContext/hooks/useGuest/index.ts
@@ -1,51 +1,48 @@
 import { ChangeEvent, useCallback, useMemo, useState } from "react"
 
 import { BaseBookingType, BookingRoomType, SimpleBookingType } from "@/types/room"
-
 import useDocument from "@/hooks/useDocument"
 
 import { isValidName } from "@/validation/user"
+import { defaultInputField } from "@/config/input"
+import useContact from "@/hooks/useContact"
+import { GuestType } from "@/types/guest"
 
 type ChangeNameKeyType = "first" | "last"
 
-const initialInput = {
-    error: false,
-    helperText: "",
-    value: ""
-}
-
 const initial = {
-    first: structuredClone(initialInput),
-    last: structuredClone(initialInput)
+    first: structuredClone(defaultInputField),
+    last: structuredClone(defaultInputField)
 }
 
 const useGuest = () => {
     const [ name, setName ] = useState(initial)
 
+    const { getContact, ...contactRest } = useContact()
     const { document, ...documentRest } = useDocument()
+
+    const contact = getContact()
+    const hasContactErrors = contactRest.hasErrors
+    const hasDocumentErrors = documentRest.hasErrors()
 
     const hasErrors = useMemo(
         () => {
-            return Boolean([
-                name.first,
-                name.last,
-                document.expireDate,
-                document.issueDate,
-                document.number,
-                document.type
-            ].find(
-                input => {
-                    let hasValueError = false;
+            const fieldWithErrors =  [ name.first, name.last]
+                .find(
+                    input => {
+                        let hasValueError = false;
 
-                    if(typeof input.value === "string") hasValueError = !Boolean(input.value.trim());
-                    
-                    return input.error || hasValueError;
-                }
-            ))
+                        if(typeof input.value === "string") hasValueError = !Boolean(input.value.trim());
+                        
+                        return input.error || hasValueError;
+                    }
+                )
+            
+            return hasContactErrors || hasDocumentErrors || Boolean(fieldWithErrors)        
         },
-        [ document, name ]
+        [ hasContactErrors, hasDocumentErrors, name ]
     )
-
+   
     const changeName = useCallback((key: ChangeNameKeyType) => (e: ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
 
@@ -61,26 +58,41 @@ const useGuest = () => {
         }));
     }, []);
 
+    const resetContact = contactRest.resetContact;
     const resetDocument = documentRest.resetDocument;
 
     const reset = useCallback(
         () => {
             setName(initial);
+            resetContact()
             resetDocument();
         },
-        [ resetDocument ]
+        [ resetContact, resetDocument ]
     );
 
+    const toLiteralObject = (): GuestType => {
+        return {
+            contact: contactRest.toLiteralObject(),
+            document: documentRest.toLiteralObject(),
+            firstName: name.first.value,
+            id: null,
+            lastName: name.last.value
+        }
+    }
+
     return {
+        ...contactRest,
         ...documentRest,
         hasErrors,
         guest: {
+            contact,
             document,
             firstName: name.first,
             lastName: name.last
         },
         changeName,
-        reset
+        reset,
+        toLiteralObject
     }
 }
 
