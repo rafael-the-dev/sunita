ce9e581b15d4275aa3704bd518a95b00f4b2bf29
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-20 07:38:19 +0100

    Create clientId route, and PUT method handler

---
+++ src/app/api/stores/[storeId]/clients/[clientId]/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import { CustomerType } from "@/types/guest"
+import { User, UserType } from "@/types/user"
+
+import { apiHandler } from "@/middlewares/route-handler";
+
+import Customer from "@/models/server/db/Customer";
+import { URLParamsType } from "@/types/app-config-server";
+
+/*export const GET = async (req: NextRequest) => {
+    return await apiHandler(
+        req, 
+        async (config) => {
+            const customers = await Customer.getAll({}, config);
+
+            return NextResponse.json(customers);
+        }
+    );
+};*/
+
+export const PUT = async (req: NextRequest, { params: { clientId }}: URLParamsType) => {
+    const customer = await req.json() as CustomerType;
+    
+    return await apiHandler(
+        req,
+        async (config) => {
+            await Customer.update({ ...customer, id: clientId }, config);
+
+            return NextResponse.json({}, { status: 201 });
+        }
+    )
+};
