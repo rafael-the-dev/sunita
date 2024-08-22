481d52efb5b7fba5e55c1e54780fef529b1dc257
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-22 07:45:17 +0100

    Create new Input component - InsertInput that receive receives user input then add it to array list

---
+++ src/components/Input/InsertInput/index.tsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+import classNames from "classnames"
+import Avatar from "@mui/material/Avatar"
+import IconButton from "@mui/material/IconButton"
+
+import AddIcon from "@mui/icons-material/Add"
+
+type PropsType = {
+    onInsert: (image: string) => void;
+}
+
+const InsertInput = ({ onInsert }: PropsType) => {
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    const clickHandler = () => {
+        const value = inputRef.current?.value;
+
+        if(!value?.trim()) return;
+
+        onInsert(value)
+
+        inputRef.current.value = "";
+    }
+
+    return (
+        <div className="border border-solid border-gray-300 flex items-center py-1 pl-2 pr-1">
+            <input 
+                className="border-0 font-normal grow outline-none text-small"
+                placeholder="Insert image link"
+                ref={inputRef}
+            />
+            <IconButton
+                onClick={clickHandler}
+                type="button">
+                <AddIcon />
+            </IconButton>
+        </div>
+    )
+}
+
+export default InsertInput
