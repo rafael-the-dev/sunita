76e41cb9dd7c53e9b20763a637e020ac824d8de6
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-25 19:59:49 +0100

    Create new bookings route

---
+++ src/app/api/stores/[storeId]/properties/bookings/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server"
+
+import { BookingType } from "@/types/booking"
+import { URLParamsType } from "@/types/app-config-server"
+
+import { apiHandler } from "@/middlewares/route-handler"
+
+import Booking from "@/models/server/db/Booking"
+
+export const GET = (req: NextRequest, { params: { storeId } }: URLParamsType) => {
+    return apiHandler(
+        req,
+        async ({ mongoDbConfig, user }) => {
+            const rooms = await Booking.getAll(
+                { 
+                    filters: {
+                        owner: storeId
+                    } 
+                },
+                { 
+                    mongoDbConfig, 
+                    user 
+                }
+            )
+
+            return NextResponse.json(rooms)
+        }
+    )
+}
+
+export const POST = (req: NextRequest, { params }: URLParamsType) => {
+    return apiHandler(
+        req,
+        async ({ mongoDbConfig, user }) => {
+            const booking = await req.json() as BookingType
+
+            await Booking.register(booking, { mongoDbConfig, user })
+
+            return NextResponse.json(
+                { message: "Booking was successfully created" }, 
+                { status: 201 }
+            )
+        }
+    )
+}
