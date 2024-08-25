da167db76b44662c1fa80714b658fe0d739f50ed
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-25 19:40:23 +0100

    Create new type - FetchResponseType that stands for response of the useFetch hook

--- src/hooks/useFetch/types.ts
+++ src/hooks/useFetch/types.ts
@@ -4,3 +4,8 @@ type OnErrorType = (error: Error) => void;
 type OnSuccessType = <T>(res: Response, data: T) => void;
 export type FetchDataPropsType = { options?: RequestInit, onError?: OnErrorType, onSuccess?: OnSuccessType, path?: string, signal?: AbortSignal };
 export type FetchDataFuncType = ({ options, onError, onSuccess, path, signal }: FetchDataPropsType) => Promise<void>;
+export type FetchResponseType<T> = {
+    data: T,
+    error: Error,
+    fetchData: FetchDataFuncType
+}
