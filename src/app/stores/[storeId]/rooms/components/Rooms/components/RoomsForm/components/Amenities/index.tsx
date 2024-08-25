a38dbd595fbeef9413c9a98fc2148e29acf56622
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-25 20:09:30 +0100

    Render suggested amenities and highlighted selected ones

--- src/app/stores/[storeId]/rooms/components/Rooms/components/RoomsForm/components/Amenities/index.tsx
+++ src/app/stores/[storeId]/rooms/components/Rooms/components/RoomsForm/components/Amenities/index.tsx
@@ -1,5 +1,10 @@
 
+import classNames from "classnames";
 import Chip from "@mui/material/Chip"
+
+import { AMENITIES } from "@/types/property";
+
+import { getList } from "@/helpers";
 
 import InsertInput from "@/components/Input/InsertInput"
 import Legend from "@/components/shared/Legend"
@@ -10,9 +15,31 @@ type PropsType = {
     onRemove: (amenity: string) => void;
 }
 
+const amenitiesList = getList(AMENITIES)
+const amenitiesListClone = structuredClone(amenitiesList)
+
 const Amenities = ({ list, onInsert, onRemove }: PropsType) => {
 
     const deleteHandler = (amenity: string) => () => onRemove(amenity);
+
+    const getSuggestionList = () => {
+        const mappedList = list
+            .map(amenity => ({ label: amenity, value: amenity }))
+            .filter(amenity => amenitiesList.find(item => item.value !== amenity))
+
+        return amenitiesList
+            .map(item => ({
+                isSelected: list.includes(item.value),
+                label: item.value
+            }))
+    }
+
+    const getSelectedAmenitiesList = () => {
+        return list
+            .filter(amenity => !Boolean(amenitiesList.find(item => item.value === amenity)))
+    }
+
+    const clickHandler = (item: string, isSelected: boolean) => () => isSelected ? onRemove(item) : onInsert(item)
 
     return (
         <fieldset>
@@ -22,7 +49,22 @@ const Amenities = ({ list, onInsert, onRemove }: PropsType) => {
             <InsertInput onInsert={onInsert}/>
             <ul className="flex flex-wrap gap-x-3 mt-3">
                 {
-                    list
+                    getSuggestionList()
+                        .map(({ isSelected, label }) => (
+                            <button
+                                className={classNames(
+                                    isSelected ? "bg-gray-200 border-gray-300" : "bg-white ",
+                                    `border-gray-200 cursor-pointer outline-none px-2.5 py-1 rounded-full shadow-none text-black`
+                                )}
+                                key={label}
+                                onClick={clickHandler(label, isSelected)}
+                                type="button">
+                                { label }
+                            </button>
+                        ))
+                }
+                {
+                    getSelectedAmenitiesList()
                         .map(amenity => (
                             <Chip 
                                 key={amenity}
