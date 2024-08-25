b8b9f95bb82726c4e3a584999c26ce5df8a331a7
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-25 19:55:00 +0100

    Create new validation file for the server operations

---
+++ src/models/server/db/validation/index.ts
@@ -0,0 +1,10 @@
+
+import InvalidArgumentError from "@/errors/server/InvalidArgumentError"
+
+export const validateArray = (key: string, list: Object) => {
+    if(!list || !Array.isArray(list)) throw new InvalidArgumentError(`Invalid ${key}`)
+}
+
+export const validateObject = (key: string, obj: Object) => {
+    if(!obj || typeof obj !== "object") throw new InvalidArgumentError(`nvalid ${key}`)
+}
