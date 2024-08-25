daa0cbf7f70869cefca9c3abdec3ed9825b234f1
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-25 19:57:01 +0100

    Add image column to the table, and change pricing column type

--- src/views/Booking/components/Rooms/index.tsx
+++ src/views/Booking/components/Rooms/index.tsx
@@ -1,13 +1,15 @@
 import { useCallback, useRef } from "react"
+import Avatar from "@mui/material/Avatar"
 
 import { RoomType } from "@/types/room"
 import { TableHeadersType } from "@/components/table/types"
 
 import Status from "@/components/shared/Status"
 import Table from "@/components/shared/table"
+import { PropertyType } from "@/types/property"
 
 type PropsType = {
-    data: RoomType[],
+    data: PropertyType[],
     onClickTableRow: (row: RoomType) => () => void
 }
 
@@ -15,34 +17,73 @@ const RoomsContainer = ({ data, onClickTableRow }: PropsType) => {
     const tableHeaders = useRef<TableHeadersType[]>(
         [
             {
+                getComponent({ item }) {
+                    const property = item as PropertyType
+
+                    return (
+                        <div className="flex justify-center w-full">
+                            <Avatar 
+                                alt={property.name} 
+                                src={property?.images[0]}  
+                                sx={{ height: 47, width: 47 }}
+                                variant="square"
+                            />
+                        </div>
+                    )
+                },
+                label: "Image",
+                key: {
+                    value: "images"
+                }
+            },
+            {
+                label: "Name",
+                key: {
+                    value: "name"
+                }
+            },
+            {
                 label: "Type",
                 key: {
                     value: "type"
                 }
             },
             {
-                label: "Number of rooms",
-                key: {
-                    value: "quantity"
-                }
-            },
-            {
                 label: "Hourly price",
                 key: {
-                    value: "hourlyPrice"
+                    value: "price",
+                    subKey: {
+                        value: "hourly"
+                    }
                 }
             },
             {
                 label: "Daily price",
                 key: {
-                    value: "dailyPrice"
+                    value: "price",
+                    subKey: {
+                        value: "daily"
+                    }
+                }
+            },
+            {
+                label: "Price per night",
+                key: {
+                    value: "price",
+                    subKey: {
+                        value: "night"
+                    }
                 }
             },
             {
                 getComponent({ item }) {
-                    const room = item as RoomType
+                    const property = item as PropertyType
 
-                    return <Status status={room.status}  />
+                    return (
+                        <div className="flex justify-center w-full">
+                            <Status status={property.status}  />
+                        </div>
+                    )
                 },
                 label: "Status",
                 key: {
