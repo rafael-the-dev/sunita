0da47f2d3b792d508a126358569247f2d5eaa8cc
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-27 16:12:27 +0100

    Refactor code using window.locantion.search API to retrieve all search params

--- src/components/shared/filters/components/submit-button/index.tsx
+++ src/components/shared/filters/components/submit-button/index.tsx
@@ -1,52 +1,21 @@
 import { useContext } from "react"//:;
 
 import { FiltersContext } from "@/context/FiltersContext"
-import useSearchParamsHook from "@/hooks/useSearchParams";
 
 import Button from "@/components/shared/button";
-
-const mapValues = (key: string, list: string[]) => list.map(item => `${key}=${item}&`);
 
 const SubmitButton = () => {
     const { fetchData, loading, url } = useContext(FiltersContext)
 
-    const searchParams = useSearchParamsHook();
-
     const getSearchParams = () => {
-        let queryParams = ""
-
-        const users = searchParams.getAll("user");
-        if(users.length > 0) queryParams += mapValues("user", users).join("");
-
-        const products = searchParams.getAll("product");
-        if(products.length > 0) queryParams += mapValues("product", products).join("");
-
-        const startDate = searchParams.get("start-date", "")
-        if(startDate) queryParams += `start-date=${startDate}&`
-
-        const category = searchParams.get("category", "")
-        if(category) queryParams += `category=${category}&`
-
-        const endDate = searchParams.get("end-date", "");
-        if(startDate && endDate) queryParams += `end-date=${endDate}`
-
-        return queryParams;
+        const params = new URLSearchParams(window.location.search)
+     
+        return params.toString();
     };
-
-    const hasSearchParams = () => {
-        const hasUsersSearchParam = searchParams.getAll("user").length > 0;
-        const hasProductsSearchParam = searchParams.getAll("product").length > 0;
-        const hasStartDate = searchParams.get("start-date", "");
-        const hasEndDate = searchParams.get("end-date", "") && hasStartDate;
-
-        return hasProductsSearchParam || hasUsersSearchParam || hasStartDate || hasEndDate;
-    };
-
-    const canISubmit = hasSearchParams();
-
+    
     const submitHandler = async () => {
         await fetchData({
-            ...( canISubmit ? { path: `${url}?${getSearchParams()}` } : {})
+            path: `${url}?${getSearchParams()}`
         })
     };
 
@@ -55,7 +24,7 @@ const SubmitButton = () => {
             className="mt-4 px-6 py-2"
             disabled={loading}
             onClick={submitHandler}>
-            { loading ? "Loading..." : ( canISubmit ? "Submit" : "Refresh" )}
+            { loading ? "Loading..." : "Submit" }
         </Button>
     );
 };
