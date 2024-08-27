9fd3ddce678738d17dbb5abf5947cffbf11fa07b
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-27 16:01:26 +0100

    sort list based on the key, and set key's default value to createdAt

--- src/helpers/sort.ts
+++ src/helpers/sort.ts
@@ -1,7 +1,7 @@
 
 
-export const sort = <T extends { createdAt: string | Date } >(list: T[]) => {
+export const sort = <T extends { createdAt: string | Date } >(list: T[], key="createdAt") => {
     list.sort((a, b) => {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+        return new Date(b[key]).getTime() - new Date(a[key]).getTime();
     });
 }
