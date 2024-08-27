34cd86b7bb6a831697fc68dcf55659a9e2678595
Rafael Tivane <luislangabusiness@gmail.com>
2024-08-27 16:02:45 +0100

    define booking's status, add code, createdAt and status fields to booking type, and finally create booking response type

--- src/types/booking.ts
+++ src/types/booking.ts
@@ -3,13 +3,22 @@ import { GuestType } from "./guest";
 import { ProductPayment as PaymentMethod } from "./payment-method"
 import { PropertyType } from "./property";
 
+export enum BOOKING_STATUS {
+    CONFIRMED = "confirmed",
+    CANCELLED = "cancelled",
+    PENDING = "pending"
+}
+
 export type BaseBookingType = {
+    code: string,
+    createdAt: string | Date;
     checkIn: string | Date;
     checkOut: string | Date;
     date: string | Date;
     id: String,
     owner: String,
     payment: PaymentMethod,
+    status: BOOKING_STATUS
     type: BOOKING_TYPE,
     totalPrice: number
 }
@@ -27,4 +36,9 @@ export type BookingDBType = BaseBookingType & {
 export type BookingInfoType = BaseBookingType & {
     guest: GuestType,
     property: PropertyType
+}
+
+export type BookingsResponseType = {
+    list: BookingInfoType[],
+    total: number
 }
