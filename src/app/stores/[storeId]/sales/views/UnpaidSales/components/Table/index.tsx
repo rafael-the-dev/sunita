d32be7f2b4c53bbbded9c1814c58d1f89742bcf4
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-02 19:08:44 +0100

    Show debt data, when row is clicked

--- src/app/stores/[storeId]/sales/views/UnpaidSales/components/Table/index.tsx
+++ src/app/stores/[storeId]/sales/views/UnpaidSales/components/Table/index.tsx
@@ -3,12 +3,19 @@ import * as React from "react"
 import { SaleDebtInfoType } from "@/types/sale"
 import { TableHeadersType } from "@/components/table/types"
 
+import { AppContext } from "@/context/AppContext"
+import { LoginContext } from "@/context/LoginContext"
 import { SalesContext } from "@/app/stores/[storeId]/sales/context"
+import { FixedTabsContext as StaticTabsContext } from "@/context/FixedTabsContext";
 
-import Table from "@/components/shared/table"
+import SaleDetails from "@/components/dialog/sale-details";
+import Table from "@/components/shared/table";
 
 const TableContainer = () => {
-    const { unpaidSales } = React.useContext(SalesContext)
+    const { fetchDataRef } = React.useContext(AppContext);
+    const { credentials } = React.useContext(LoginContext);
+    const { unpaidSales } = React.useContext(SalesContext);
+    const { setDialog } = React.useContext(StaticTabsContext);
 
     const headers = React.useRef<TableHeadersType[]>(
         [
@@ -65,12 +72,36 @@ const TableContainer = () => {
         ]
     )
 
+    const fetchUnpaidSales = unpaidSales?.data?.fetchData;
+
+    const rowClickHandler = React.useCallback(
+        (unpaidSale: SaleDebtInfoType) => () => {
+            fetchDataRef.current = fetchUnpaidSales;
+
+            setDialog(
+                {
+                    header: {
+                        title: "Unpaid sale details"
+                    },
+                    body: (
+                        <SaleDetails 
+                            initial={{ ...unpaidSale, id: unpaidSale.id }}
+                            url={`/api/stores/${credentials?.user?.stores[0]?.storeId}/sales/debts`}
+                        />
+                    )
+                }
+            )
+        },  
+        [ credentials, fetchDataRef, fetchUnpaidSales, setDialog ]
+    )
+
     const list = unpaidSales?.data?.data ?? []
 
     return (
         <Table 
             data={list}
             headers={headers}
+            onClickRow={rowClickHandler}
         />
     )
 }
