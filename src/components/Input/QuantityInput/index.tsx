03d40d6cdd54e7f33eeb8144f8847f96b0c74d7d
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-02 19:12:16 +0100

    Move quantity input to Input folder

---
+++ src/components/Input/QuantityInput/index.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import classNames from "classnames";
+
+import styles from "./styles.module.css";
+
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+type PropsType = {
+    onDecrement: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onIncrement: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    hasError: boolean;
+    value: string | number;
+}
+
+const Container = ({ hasError, onChange, onDecrement, onIncrement, value }: PropsType) => {
+
+    // add 1 to incriment, or -1 to decrement
+
+    const errorClasses = { "text-red-600": hasError };
+
+    return (
+        <div className="flex items-center justify-center">
+            <IconButton 
+                className={classNames("p-0 text-sm", errorClasses)}
+                onClick={onDecrement}>
+                <RemoveIcon className="text-base" />
+            </IconButton>
+            <input 
+                className={classNames(styles.input, errorClasses,
+                "bg-transparent border-0 text-center outline-none")}
+                onChange={onChange}
+                value={value}
+            />
+            <IconButton 
+                className={classNames("p-0 text-sm", errorClasses)}
+                onClick={onIncrement}>
+                <AddIcon className="text-base" />
+            </IconButton>
+        </div>
+    );
+};
+
+export default Container;
