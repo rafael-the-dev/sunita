7b8901a9653b982760e942537a7d6155c377ff3f
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-02 13:38:05 +0100

    Enhance component to allow setting value to search params, and include renderList value to conditional state

--- src/components/shared/Search/SearchWithOptions/index.tsx
+++ src/components/shared/Search/SearchWithOptions/index.tsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper"
 
 import styles from "./styles.module.css"
 
+import useSearchParams from "@/hooks/useSearchParams"
+
 import Input from "@/components/Textfield"
 import Table from "@/components/shared/table"
 import { TableHeadersType } from "@/components/table/types"
@@ -23,37 +25,64 @@ type PropsType<T extends ItemType> = {
         label?: string,
         placeholder?: string
     },
-    onClickRow?: (row: T) => void 
+    onClickRow?: (row: T) => void,
+    searchParam?: boolean
 }
 
 const Container = React.forwardRef<HTMLInputElement, PropsType<ItemType>>(
-    <T extends ItemType,>({ classes, headers, getList, input, onClickRow }: PropsType<T>, ref: React.Ref<HTMLInputElement>) => {
+    <T extends ItemType,>({ classes, headers, getList, input, onClickRow, searchParam }: PropsType<T>, ref: React.Ref<HTMLInputElement>) => {
         const [ value, setValue ] = React.useState("")
 
-        const barCodeChangeHandler = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value), []);
+        const renderList = React.useRef(false)
 
-        const handleClickAway = React.useCallback(() => setValue(""), []);
+        const searchParams = useSearchParams()
+        const searchQueryString = (searchParams.get("search", "") as string).replaceAll("-", " ")
+        const currentValue = searchParam ? searchQueryString : value
 
-        const list = getList(value)
+        const valueChangeHandler = React.useCallback(
+            (e: React.ChangeEvent<HTMLInputElement>) => {
+                const { value } = e.target;
+
+                renderList.current = true
+
+                if(searchParam) searchParams.setSearchParam("search", value);
+                else setValue(value);
+            }, 
+            [ searchParam, searchParams ]
+        );
+
+        const closeHandler = React.useCallback(
+            () => {
+                if(!searchParam) setValue("");
+            },
+            [ searchParam ]
+        )
+
+        const handleClickAway = React.useCallback(() => closeHandler(), [ closeHandler ]);
+
+        const list = getList(currentValue);
 
         const rowClickHandler = React.useCallback(
             (customer: T) => () => {
-                onClickRow(customer)
-                setValue("")
-            },
-            [ onClickRow ]
-        )
+                onClickRow(customer);
 
+                renderList.current = false
+
+                closeHandler();
+            },
+            [ closeHandler, onClickRow ]
+        )
+        
         return (
             <div className={classNames(classes?.root, "input relative w12 xl:mb-0")}>
                 <Input 
                     className="w-full"
                     inputRef={ref}
-                    onChange={barCodeChangeHandler}
-                    value={value}
+                    onChange={valueChangeHandler}
+                    value={currentValue}
                     { ...(input ?? {}) }
                 />
-                { Boolean(value.trim()) && (list.length > 0) && (
+                { Boolean(currentValue.trim()) && (list.length > 0) && renderList.current && (
                     <ClickAwayListener onClickAway={handleClickAway}>
                         <Paper 
                             className={classNames(styles.tableContainer, "absolute bottom-0 left-0 overflow-y-auto w-full z-10")}
