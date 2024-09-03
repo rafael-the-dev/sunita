c1dc8578426d0e64b6d7cc55197e14fcc7ef8ab1
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-03 12:32:46 +0100

    replace Payment's table by default Payment container

--- src/components/dialog/sale-details/components/payment-methods/index.tsx
+++ src/components/dialog/sale-details/components/payment-methods/index.tsx
@@ -1,57 +1,21 @@
 import * as React from "react";
 
 import { SaleDetailsContext } from "../../context";
-import { TableHeadersType } from "@/components/table/types";
 
-import Select from "@/components/shared/combobox";
-import Table from "@/components/shared/table";
-import { PaymentMethodType } from "@/types/payment-method";
-import { paymentMethodsList } from "@/config/payment-methods";
+import PaymentList from "@/components/Container/Payment"
 
 const PaymentMethods = () => {
-    const {getSaleDetails } = React.useContext(SaleDetailsContext);
-    const { paymentMethods } = getSaleDetails();
-
-    const headers = React.useRef<TableHeadersType[]>([
-        {
-            label: "Payment method",
-            getComponent: ({ item }) => {
-                const paymentMethod = item as PaymentMethodType;
-                
-                return (
-                    <Select 
-                        className="mb-0"
-                        list={paymentMethodsList}
-                        onChange={() => {}}
-                        value={ paymentMethod.id }
-                    />
-                )
-            },
-            key: {
-                value: ""
-            }
-        },
-        {
-            label: "Amount",
-            key: {
-                value: "amount"
-            }
-        },
-        {
-            label: "Transaction ID",
-            key: {
-                value: "amount"
-            }
-        },
-    ]);
+    const { payment, paymentHandlers } = React.useContext(SaleDetailsContext);
 
     return (
         <div>
-            <div>
-                <Table 
-                    data={paymentMethods}
-                    headers={headers}
-                />
+            <div className="flex flex-col gap-y-6 items-stretch">
+                <div className="flex flex-col items-stretch">
+                    <PaymentList 
+                        { ...paymentHandlers }
+                        payment={payment}
+                    />
+                </div>
             </div>
         </div>
     )
