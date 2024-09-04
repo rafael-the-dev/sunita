a338b5c0604a4fcc04d6da08b9887954cd8c642b
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-04 15:19:18 +0100

    User default RootLayout

--- src/app/(client)/search/layout.tsx
+++ src/app/(client)/search/layout.tsx
@@ -1,42 +1,5 @@
 "use client"
 
-import { ReactNode } from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import RootLayout from "@/components/shared/layout"
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-import "@/styles/reset.css";
-
-import 'devextreme/dist/css/dx.light.css';
-
-import "@/styles/tailwind.css";
-import "@/styles/globals.css";
-
-import { theme } from "@/components/shared/layout/mui/theme";
-import { AppContextProvider } from "@/context/AppContext";
-
-const RootLayout = ({ children }: { children: ReactNode }) => {
-    return (
-        <html lang="en">
-            <body>
-                <AppRouterCacheProvider options={{  enableCssLayer: true  }}>
-                    <ThemeProvider theme={theme}>
-                        <LocalizationProvider dateAdapter={AdapterMoment}>
-                            <AppContextProvider>
-                                { children }
-                            </AppContextProvider>
-                        </LocalizationProvider>
-                    </ThemeProvider>
-                </AppRouterCacheProvider>
-            </body>
-        </html>
-    );
-};
-
-export default RootLayout;
+export default RootLayout
