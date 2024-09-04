f00a9fc3ae7631fa14fbd40bf364fc9eb7a9d963
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-04 15:30:11 +0100

    Enhance public routes test by providing more details about the path and public methods

---
+++ src/middlewares/api.ts
@@ -0,0 +1,35 @@
+import { NextRequest } from "next/server"
+
+export const isPublicPath = (req: NextRequest) => {
+    //replace domain and query params
+    const pathname = req
+        .url
+        .replace("http://localhost:3000/api", "")
+        .replace( /\?[A-z0-9.+-=]*/g,"");
+
+    const publicResources = [
+        {
+            methods: [ "POST" ],
+            path: "/auth/login"
+
+        },
+        {
+            methods: [ "GET" ],
+            path: "/auth/refresh"
+
+        },
+        {
+            methods: [ "GET" ],
+            path: "/stores/properties"
+
+        }
+    ]
+
+    const resource = publicResources.find(
+        ({ methods, path }) => {
+            return (path === pathname) && (methods.includes(req.method))
+        }
+    )
+    
+    return Boolean(resource)
+}
