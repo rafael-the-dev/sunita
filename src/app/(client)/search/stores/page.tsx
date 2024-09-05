5af9117624b17eee938f8cc4454c29c682634a38
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-05 05:40:34 +0100

    Remove unnecessary code - getStores is not used here

--- src/app/(client)/search/stores/page.tsx
+++ src/app/(client)/search/stores/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-import { useContext } from "react"
 import classNames from "classnames"
 
 import styles from "./styles.module.css"
 
-import { StoresContext, StoresContextProvider } from "./context"
+import { StoresContextProvider } from "./context"
 
 import Filters from "./components/Filters"
 import List from "./components/List"
@@ -13,8 +12,6 @@ import SearchBox from "./components/Searchbox"
 import Tabs from "./components/Tabs"
 
 const StoresPage = () => {
-    const { getStores } = useContext(StoresContext)
-
     return (
         <div>
             <main className={classNames(styles.container, "gap-x-12 items-stretch px-[5%] pt-8 pb-8 lg:flex")}>
