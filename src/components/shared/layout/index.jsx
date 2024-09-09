147690c489f55af9b95b7a027f4f49ea1eb61ab9
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-09 14:56:36 +0100

    import Swiper css file

--- src/components/shared/layout/index.jsx
+++ src/components/shared/layout/index.jsx
@@ -14,6 +14,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import 'devextreme/dist/css/dx.light.css';
+import 'swiper/css';
 
 import "@/styles/tailwind.css";
 import "@/styles/globals.css";
