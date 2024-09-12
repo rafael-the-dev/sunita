c8911ac119275f0089d21d761bcb960849a427c2
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-12 06:39:30 +0100

    add quick and support links, then change layout

--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -7,19 +7,35 @@ import XIcon from '@mui/icons-material/X';
 import Link from "@/components/link"
 import ListItem from "./components/ListItem"
 import SocialMedia from "./components/SocialMedia"
+import Title from "./components/Title"
 
-const links = [
+const quickLinks = [
     {
         href: "/",
         label: "Home"
     },
     {
-        href: "/about-us",
+        href: "/",
         label: "About us"
     },
     {
-        href: "/pricing",
+        href: "/",
         label: "Pricing"
+    }
+]
+
+const supportLinks = [
+    {
+        href: "/",
+        label: "Help center"
+    },
+    {
+        href: "/",
+        label: "Privacy policy"
+    },
+    {
+        href: "/",
+        label: "Terms of service"
     }
 ]
 
@@ -47,27 +63,48 @@ const Footer = () => {
 
     return (
         <footer
-            className="bg-primary-800 pt-8 pb-4 px-[5%] text-white">
+            className="bg-primary-800 pt-8 pb-4 px-[5%] text-white md:pt-12">
             <div className="flex flex-col sm:flex-row sm:justify-between">
-                <div className="sm:grow">
+                <div className="sm:grow sm:flex">
                     <Link
                         className="no-underline text-white text-4xl !uppercase" 
                         href="/">
                         Sunita
                     </Link>
-                    <ul className='flex flex-col gap-y-3 mt-6 sm:flex-row sm:gap-x-3 sm:gap-y-0'>
-                        {
-                            links.map(
-                                ({ href, label }) => (
-                                    <ListItem
-                                        href={href}
-                                        key={href}>
-                                        { label }
-                                    </ListItem>
-                                )
-                            )
-                        }
-                    </ul>
+                    <div className="flex flex-col gap-y-4 mt-8 sm:mt-0 sm:flex-row sm:gap-y-0 sm:ml-6 md:ml-12">
+                        <div className="flex flex-col">
+                            <Title>Quick links</Title>
+                            <ul className='flex flex-col gap-y-3 mt-6'>
+                                {
+                                    quickLinks.map(
+                                        ({ href, label }) => (
+                                            <ListItem
+                                                href={href}
+                                                key={href}>
+                                                { label }
+                                            </ListItem>
+                                        )
+                                    )
+                                }
+                            </ul>
+                        </div>
+                        <div className="flex flex-col sm:ml-6 md:ml-12">
+                            <Title>Support links</Title>
+                            <ul className='flex flex-col gap-y-3 mt-6'>
+                                {
+                                    supportLinks.map(
+                                        ({ href, label }) => (
+                                            <ListItem
+                                                href={href}
+                                                key={href}>
+                                                { label }
+                                            </ListItem>
+                                        )
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <ul className="flex flex-row gap-x-3 mt-12 sm:mt-0">
                     {
@@ -83,7 +120,7 @@ const Footer = () => {
                     }
                 </ul>
             </div>
-            <div className="border-t border-solid border-gray-50 justify-center mt-6 opacity-80 py-6 flex">
+            <div className="border-t border-solid border-gray-50 justify-center mt-10 opacity-80 py-6 flex">
                 {
                     new Date(Date.now()).getFullYear()
                 }
