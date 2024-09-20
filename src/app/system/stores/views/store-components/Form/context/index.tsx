e7d378c39448d78653175288b8ba22311c922f01
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-20 12:55:52 +0100

    Create form's context

---
+++ src/app/system/stores/views/store-components/Form/context/index.tsx
@@ -0,0 +1,94 @@
+import * as React from "react"
+
+import { EnrollStoreType,  } from "@/types/warehouse"
+import { FormContextType, PropsType } from "./types"
+
+import useAddress from "@/hooks/useAddress"
+import useContact from "@/hooks/useContact"
+import usePayment from "./hooks/usePayment"
+import useUser from "./hooks/useUser"
+
+import { isValidName } from "@/validation/product"
+
+import { defaultInputField } from "@/config/input"
+
+export const FormContext = React.createContext<FormContextType>({} as FormContextType)
+
+const initialBaseDetails = {
+    name: defaultInputField,
+}
+
+export const FormContextProvider = ({ children }: PropsType) => {
+    const [ baseDetails, setBaseDetails ] = React.useState(initialBaseDetails)
+
+    const address = useAddress()
+    const contact = useContact()
+    const payment = usePayment(8500)
+    const user = useUser()
+
+    const hasErrors = () => {
+        return [
+            baseDetails.name.error || !baseDetails.name.value.trim(),
+            contact.hasErrors,
+            payment.hasErrors,
+            user.hasErrors()
+        ].find(error => error)
+    }
+
+    const nameChangeHandler = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target
+
+            const hasError = !isValidName(value)
+
+            setBaseDetails(details => ({
+                ...details,
+                name: {
+                    error: hasError,
+                    helperText: hasError ? "Invalid name" : "",
+                    value
+                }
+            }))
+        },
+        []
+    )
+
+    const reset = () => {
+        setBaseDetails(structuredClone(initialBaseDetails))
+        contact.resetContact()
+        payment.reset()
+        user.reset()
+    }
+
+    const toLiteralObject = () => {
+        const storeEnrollment: EnrollStoreType = {
+            address: address.toLiteralObject(),
+            contact: contact.toLiteralObject(),
+            id: null,
+            name: baseDetails.name.value,
+            payment: payment.getPayment(),
+            status: null,
+            users: [ user.toLiteralObject() ]
+        }
+
+        return storeEnrollment
+    }
+
+    return (
+        <FormContext.Provider
+            value={{
+                ...baseDetails,
+                address,
+                contact,
+                payment,
+                user,
+
+                hasErrors,
+                nameChangeHandler,
+                reset,
+                toLiteralObject
+            }}>
+            { children }
+        </FormContext.Provider>
+    )
+}
---
+++ src/app/system/stores/views/store-components/Form/context/types.ts
@@ -0,0 +1,47 @@
+import { ChangeEvent, ReactNode } from "react"
+
+import { AddressEventHandlers, AddressInputType } from "@/hooks/useAddress/types"
+import { ContactInputType, ContactMethodsType } from "@/hooks/useContact/types"
+import { DocumentInputFunctions } from "@/hooks/useDocument/types"
+import { EnrollStoreType } from "@/types/warehouse"
+import { PaymentType } from "@/types/payment-method"
+import { PaymentFunctionsType } from "@/hooks/usePayment/types"
+
+import { defaultInputField } from "@/config/input"
+
+type InputType = typeof defaultInputField
+
+type AddressType = AddressEventHandlers & {
+    getAddress: () => AddressInputType
+}
+
+type ContactType = ContactMethodsType & {
+    getContact: () => ContactInputType
+}
+
+export type FormContextType = {
+    address: AddressType,
+    contact: ContactType,
+    name: InputType,
+    payment: PaymentFunctionsType & {
+        getPayment: () => PaymentType
+    },
+    user: {
+        address: AddressType,
+        changeName: (key: "firstName" | "lastName") => (e: ChangeEvent<HTMLInputElement>) => void,
+        contact: ContactType,
+        document: DocumentInputFunctions,
+        firstName: InputType,
+        lastName: InputType
+    },
+
+    
+    hasErrors: () => boolean,
+    nameChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    reset: () => void,
+    toLiteralObject: () => EnrollStoreType,
+}
+
+export type PropsType = {
+    children: ReactNode
+}
