886fae3f8aff8f5b6aba5ee3097a0e42e4853407
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-20 12:37:25 +0100

    Create Store model that handles store CRUD operations

---
+++ src/models/server/db/Store/index.ts
@@ -0,0 +1,88 @@
+
+import { ConfigType } from "@/types/app-config-server"
+import { EnrollStoreType, StoreType } from "@/types/warehouse"
+import { BaseFeeType, FEES_TYPE } from "@/types/fees"
+import { STATUS } from "@/types"
+
+import { getId } from "@/helpers/id"
+import getStoreProxy from "./proxy"
+
+import FeesModel from "../Fees"
+import UsersModel from "../Users"
+
+import InvalidArgumentError from "@/errors/server/InvalidArgumentError"
+
+
+class Store {
+    static async register(store: EnrollStoreType, config: ConfigType) {
+        if(!store || typeof store !== "object") throw new InvalidArgumentError("Invalid store details")
+
+        const id = getId()
+
+        const enrollStore: EnrollStoreType = {
+            address: null,
+            contact: null,
+            id,
+            name: null,
+            payment: null,
+            users: [],
+            status: STATUS.INACTIVE
+        }
+
+        const storeProxy = getStoreProxy(enrollStore)
+
+        storeProxy.contact = store.contact
+        storeProxy.name = store.name
+
+        const newFee: BaseFeeType = {
+            price: 0,
+            payment: store.payment,
+            storeId: config.user.stores[0].storeId,
+            total: 0,
+            type: FEES_TYPE.ENROLLMENT
+        }
+
+        await FeesModel.register(newFee, config)
+
+        if(!store.users || !Array.isArray(store.users) || store.users.length !== 1) throw new InvalidArgumentError("Admin details not provided.")
+
+        await UsersModel.register(store.users[0], config)
+
+        try {
+            const newStore: StoreType = {
+                address: null,
+                contact: storeProxy.contact,
+                clients: [],
+                expenses: [],
+                "expenses-categories": [],
+                id,
+                name: storeProxy.name,
+                products: [],
+                rooms: [],
+                "rooms-booking": [],
+                sales: [],
+                suppliers: [],
+                "stock-reports": [],
+                users: [],
+                "unpaid-sales": [],
+                status: STATUS.ACTIVE
+            }
+
+            await config
+                .mongoDbConfig
+                .collections
+                .WAREHOUSES
+                .insertOne(newStore)
+        } catch(e) {
+            await config
+                .mongoDbConfig
+                .collections
+                .WAREHOUSES
+                .deleteOne({ id })
+                
+            throw e
+        }
+    }
+}
+
+export default Store
