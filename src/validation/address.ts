2f864e934bfa8d877a30aef9151f572fa06729cd
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-20 12:46:49 +0100

    Create address validation

--- src/validation/address.ts
+++ src/validation/address.ts
@@ -14,3 +14,32 @@ export const isValidAddressStreet = (value: string) => {
 
     return Boolean(value.trim())
 }
+
+export const isValidCountry = (country: string) => {
+    return !country && [].includes(country);
+}
+
+
+export const isValidNumber = (value: string) => {
+    const pattern = /^[1-9]*$/; 
+    
+    return (value && pattern.test(value)) 
+}
+
+export const isValidState = (state: string) => {
+    const stateRegex = /^[A-Za-z\s]{2,50}$/; // Validates states as letters and spaces, 2-50 characters long
+    
+    return (state && stateRegex.test(state)) 
+} 
+
+export const isValidStreet = (street: string) => {
+    const streetRegex = /^[A-Za-z0-9\s,.-]{5,100}$/; // Allows alphanumeric characters, spaces, commas, periods, hyphens, 5-100 characters long
+    
+    return (street && streetRegex.test(street)) 
+} 
+
+export const isValidCity = (city: string) => {
+    const cityRegex = /^[A-Za-z\s]{2,100}$/; // Allows alphanumeric characters, spaces, commas, periods, hyphens, 2-100 characters long
+    
+    return (city && cityRegex.test(city)) 
+} 
