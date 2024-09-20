4ee4c2256b47ec52ead7e1c66e0420dbabe00793
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-20 12:46:29 +0100

    define late payment fine period

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,11 @@
 
+export enum LATE_PAYMENT_FINE_PERIOD {
+    DAY = "d",
+    WEEK = "w",
+    MONTH = "w",
+    YEAR = "y"
+}
+
 export enum STATUS {
     ACTIVE = "active",
     INACTIVE = "inactive",
