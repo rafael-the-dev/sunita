80549c96f0dbf03293569cd6554b81a45d33fd81
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-20 12:56:41 +0100

    create store form usePayment hook

---
+++ src/app/system/stores/views/store-components/Form/context/hooks/usePayment/index.ts
@@ -0,0 +1,100 @@
+import { useCallback, useContext, useEffect, useMemo, useState } from "react"
+
+import { ChangePaymentMethodValueType } from "@/hooks/usePayment/types";
+import { BookingInfoType } from "@/types/booking";
+import { PAYMENT_METHODS } from "@/types/payment-method";
+
+import { FixedTabsContext } from "@/context/FixedTabsContext";
+
+import usePayment from "@/hooks/usePayment"
+
+import { paymentMethodsList } from "@/config/payment-methods";
+import { calculatePayment } from "@/helpers/booking";
+import { isValidPayment } from "@/validation/payment";
+
+const initialState = {
+    changes: 0,
+    paymentMethods: [
+        {
+            amount: 0, 
+            id: paymentMethodsList[0].value,
+            transactionId: null
+        }
+    ],
+    remainingAmount: 0,
+    totalReceived: 0
+};
+
+const useEnrollmentPayment = (totalPrice: number) => {
+    const { getDialog } = useContext(FixedTabsContext);
+
+    const bookingInfo = getDialog().current?.payload as BookingInfoType
+    const hasPayload = Boolean(bookingInfo)
+
+    const [ payment, setPayment ] = useState(
+        () => {
+            if(!hasPayload) return initialState;
+
+            return {
+                ...bookingInfo.payment
+            }
+        }
+    )
+
+    const {
+        add,
+        abstractChangePaymentMethodValue,
+        abstractRemovePaymentMethod,
+        changePaymentMethodId,
+        changePaymentMethodTransactionIdValue
+    } = usePayment({ setPayment })
+
+    const hasErrors = useMemo(
+        () => !isValidPayment(payment, totalPrice),
+        [ payment, totalPrice ]
+    )
+
+    const getPayment = useCallback(() => structuredClone(payment), [ payment ])
+
+    const changePaymentMethodValue = useCallback(
+        (key: ChangePaymentMethodValueType, id: PAYMENT_METHODS, amount: number | string ) => {
+            abstractChangePaymentMethodValue(key, id, amount, (payment) => calculatePayment(payment, totalPrice))
+        }, 
+        [ abstractChangePaymentMethodValue, totalPrice ]
+    )
+
+    const removePaymentMethod = useCallback(
+        (id: PAYMENT_METHODS) => abstractRemovePaymentMethod(id, (payment) => calculatePayment(payment, totalPrice)), 
+        [ abstractRemovePaymentMethod, totalPrice ]
+    );
+
+    const reset = useCallback(
+        () => setPayment(initialState),
+        []
+    );
+
+    useEffect(
+        () => {
+            setPayment(payment => {
+                const newPayment = structuredClone(payment);
+
+                calculatePayment(newPayment, totalPrice);
+
+                return newPayment;
+            })
+        },
+        [ totalPrice ]
+    )
+
+    return {
+        hasErrors, 
+
+        addPaymentMethod: add,
+        changePaymentMethodId, changePaymentMethodValue, changePaymentMethodTransactionIdValue,
+        getPayment,
+        removePaymentMethod,
+        reset
+    }
+}
+
+export default useEnrollmentPayment
