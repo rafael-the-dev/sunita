f874dbe30d3e8e2f14d55b2989daad6e225ac7b6
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-22 21:10:15 +0100

    add new view - Fees view

--- src/app/system/stores/page.tsx
+++ src/app/system/stores/page.tsx
@@ -1,19 +1,23 @@
 "use client"
 
+import { useContext } from "react"
 import classNames from "classnames"
 
 import { FiltersContextProvider } from "@/context/FiltersContext"
 import { FixedTabsContext } from "@/context/FixedTabsContext"
 import { LoginContext } from "@/context/LoginContext"
+import { StoresContext } from "./context"
 
 import useSearchParams from "@/hooks/useSearchParams"
 
 import { Provider } from "@/components/shared/FixedTabsContainer"
 import { StoresContextProvider } from "./context"
 
+import FeesView from "./views/SubscriptionsFees"
 import StoresView from "./views"
 
 enum TABS {
+    FEES = 'fees',
     STORES = "stores"
 }
 
@@ -21,10 +25,15 @@ const tabs = [
     {
         name: "Stores",
         id: TABS.STORES
+    },
+    {
+        name: "Fees",
+        id: TABS.FEES
     }
 ]
 
 const StoresPage = () => {
+    const { fees } = useContext(StoresContext)
     const searchParams = useSearchParams()
 
     const tab = searchParams.get("tab", TABS.STORES)
@@ -33,6 +42,7 @@ const StoresPage = () => {
         <div className={classNames("scrollable overflow-x-auto px-4 pt-6")}>
             {
                 {
+                    [TABS.FEES]: <FiltersContextProvider autoFetch={false} refetchData={fees.fetchData} url="/api/stores/fees"><FeesView /></FiltersContextProvider>,
                     [TABS.STORES]: <StoresView />
                 }[tab]
             }
