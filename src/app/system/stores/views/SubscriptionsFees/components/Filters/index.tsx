f5f365c9e84379820270917758ea8b36fe6d029c
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-23 14:52:42 +0100

    import and implement Fees type filter

--- src/app/system/stores/views/SubscriptionsFees/components/Filters/index.tsx
+++ src/app/system/stores/views/SubscriptionsFees/components/Filters/index.tsx
@@ -2,31 +2,37 @@
 import useSearchParams from "@/hooks/useSearchParams"
 
 import Filters from "@/components/shared/filters"
+import FeeType from "./components/FeeType"
 import RadioGroup from "@mui/material/RadioGroup"
 import RadioButton from "@/components/radio-button"
 import Status from "./components/Status"
 
 enum FILTERS {
     DATE = 'date',
-    STATUS = "status"
+    STATUS = "status",
+    TYPE = "type"
 }
 
-const filtersList = [,
-    {
-        label: "Status",
-        value: FILTERS.STATUS
-    },
+const filtersList = [
     {
         label: "Date",
         value: FILTERS.DATE
-    }
+    },
+    {
+        label: "Status",
+        value: FILTERS.STATUS
+    },,
+    {
+        label: "Type",
+        value: FILTERS.TYPE
+    },
 ]
 
 
 const FiltersContainer = () => {
     const searchParams = useSearchParams()
 
-    const filterQueryParam = searchParams.get("filter", FILTERS.STATUS)
+    const filterQueryParam = searchParams.get("filter", FILTERS.DATE)
 
     return (
         <Filters>
@@ -46,6 +52,7 @@ const FiltersContainer = () => {
                 {
                     {
                         [FILTERS.DATE]: <Filters.Date />,
+                        [FILTERS.TYPE]: <FeeType />,
                         [FILTERS.STATUS]: <Status />
                     }[filterQueryParam]
                 }
