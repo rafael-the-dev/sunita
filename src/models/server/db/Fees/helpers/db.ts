f81859515ed47cf6321deee863347b174ee73b21
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-23 06:43:52 +0100

    Retrieve store details and creation date

--- src/models/server/db/Fees/helpers/db.ts
+++ src/models/server/db/Fees/helpers/db.ts
@@ -14,22 +14,32 @@ export const getFees = async (filters: FiltersType, config: ConfigType) => {
                 {
                     $match: filters ?? {}
                 },
-                /*{
+                {
                     $lookup: {
                         from: "warehouses",
                         foreignField: "id",
                         localField: "storeId",
                         as: "stores"
                     }
-                },*/
+                },
+                { $unwind: "$stores" },
                 {
                     $group: {
                         _id: "$id",
+                        createdAt: { $first: "$createdAt" },
                         id: { $first: "$id" },
                         latePaymentFine: { $first: "$latePaymentFine" },
                         price: { $first: "$price" },
                         payment: { $first: "$payment" },
                         //registeredBy: "",
+                        store: { 
+                            $first: {
+                                address: "$stores.address",
+                                contact: "$stores.contact",
+                                id: "$stores.id",
+                                name: "$stores.name"
+                            }
+                        },
                         status: { $first: "$status" },
                         type: { $first: "$type" },
                         total: { $first: "$total" },
