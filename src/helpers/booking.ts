f99c01e463fa78fe685cd5314fe76fde6f178923
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-25 20:46:29 +0100

    [Fix] booking helper file is loading backend code

--- src/helpers/booking.ts
+++ src/helpers/booking.ts
@@ -1,15 +1,12 @@
 import moment from "moment"
 import currency from "currency.js"
 
-import { BOOKING_TYPE, BaseBookingType, BookingRoomType, RoomType } from "@/types/room"
-import { ConfigType, FiltersType } from "@/types/app-config-server"
+import { BOOKING_TYPE, RoomType } from "@/types/room"
 import { PaymentType } from "@/types/payment-method"
 
-import { dateTimeFormat, resetTime, toISOString } from "./date"
+import { dateTimeFormat } from "./date"
 
-import Bookings from '@/models/server/db/Booking'
 import { PropertyType } from "@/types/property"
-import { BookingType, BookingInfoType } from "@/types/booking"
 
 type DateType = string | Date
 
@@ -77,92 +74,4 @@ export const calculatePayment = (payment: PaymentType, total: number) => {
     payment.changes = totalReceived > total ? Math.abs(difference) : 0;
     payment.remainingAmount = totalReceived <= total ? difference : 0;
     payment.totalReceived = totalReceived;
-}
-
-
-export const getBookings = async ({ filters }: { filters?: FiltersType }, { mongoDbConfig, user }: ConfigType) => {
-    const pipeline = [
-        {
-            $match: {
-                ...(filters ?? {}),
-            }
-        },
-        {
-            $lookup: {
-                from: "properties",
-                foreignField: "id",
-                localField: "property",
-                as: "properties",
-            }
-        },
-        {
-            $lookup: {
-                from: "guests",
-                foreignField: "id",
-                localField: "guest",
-                as: "guests",
-            }
-        },
-        {
-            $unwind: "$guests"
-        },
-        {
-            $unwind: "$properties"
-        },
-        {
-            $group: {
-                _id: "$id",
-                checkIn: { $first: "$checkIn" },
-                checkOut: { $first: "$checkOut" },
-                date: { $first: "$date" },
-                guest: { $first: "$guests" },
-                id: { $first: "$id" },
-                property: { $first: "$properties" },
-                payment: { $first: "$payment" },
-                status: { $first: "$status" },
-                type: { $first: "$type" },
-                totalPrice: { $first: "$totalPrice" }
-            }
-        }
-    ];
-
-    const bookings = await mongoDbConfig
-        .collections
-        .BOOKINGS
-        .aggregate(pipeline)
-        .toArray() as BookingInfoType[];
-
-    return bookings;
-}
-
-
-export const isBookingAvailable = async ({ checkIn, checkOut, property }: BookingType, config: ConfigType) => {
-    const date = moment(checkIn);
-    resetTime(date);
-
-    const endDateTime = moment(checkOut)
-    endDateTime.hours(23)
-    endDateTime.minutes(59)
-    endDateTime.seconds(59)
-
-    const bookings = await Bookings.getAll(
-        {
-            filters: {
-                property,
-                date: {
-                    $gte: toISOString(date),
-                    $lte: toISOString(endDateTime),
-                },
-                $or: [
-                    {
-                        checkIn: { $lt: checkOut },
-                        checkOut: { $gt: checkIn }
-                    }
-                ]
-            }
-        },
-        config
-    )
-
-    return bookings.data.list.length === 0
 }
