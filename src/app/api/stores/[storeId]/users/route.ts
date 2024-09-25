9348bb4d7c604496c02b5db8d81df9d010921ac5
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-25 21:23:38 +0100

    [fix] contact missing issue

--- src/app/api/stores/[storeId]/users/route.ts
+++ src/app/api/stores/[storeId]/users/route.ts
@@ -1,24 +1,29 @@
 import { NextResponse, NextRequest } from "next/server";
 
 import { apiHandler } from "@/middlewares/route-handler";
-import { User, UserType } from "@/types/user"
+import { User } from "@/types/user"
 
 import Users from "@/models/server/db/Users";
 
 export const GET = async (req: NextRequest) => {
-    return await apiHandler(req, async ({ mongoDbConfig, user }) => {
-        const users = await Users.getAll({}, { mongoDbConfig });
-        return NextResponse.json(users);
-    });
+    return await apiHandler(
+        req, 
+        async ({ mongoDbConfig, user }) => {
+            const users = await Users.getAll({}, { mongoDbConfig });
+            return NextResponse.json(users);
+        }
+    );
 };
 
 
 export const POST = async (req: NextRequest) => {
-    const { address, document, category, firstName, lastName, password, username } = await req.json() as User;
+    const newUser = (await req.json()) as User;
 
-    return await apiHandler(req,
+    return await apiHandler(
+        req,
         async ({ mongoDbConfig, user }) => {
-            await Users.register({  address, document, category, firstName, id: username, lastName, password, stores: [], username }, { mongoDbConfig, user });
+            await Users.register({  ...newUser, id: user.username, stores: [] }, { mongoDbConfig, user });
+
             return NextResponse.json({}, { status: 201 });
         }
     )
