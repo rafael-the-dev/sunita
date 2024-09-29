0846d72a9992c59993516858b44e270ffbbf6e9d
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-29 20:00:32 +0100

    Change Layout component into a server side component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-"use client"
-
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation"
+import {headers} from "next/headers"
 
 import Dialog from "@/components/shared/layout/components/Dialog";
 import Layout from "@/components/shared/layout";
 import ProtectedLayout from "@/components/shared/layout/ProtectedLayout";
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-    const pathname = usePathname()
+import PropertiesSSR from "./ServerSideContainers/Properties"
+
+const RootLayout = ({ children, queryParams }: { children: ReactNode, queryParams: { [key: string]: string | string[] } }) => {
+    const headersList = headers()
+    const pathname = headersList.get("current-pathname")
 
     const isProtectedRoute = (
         () => {
             const protectedRoutes = [ "/stores", "/system", "/login" ]
 
-            const isProtected = Boolean(protectedRoutes.find(route => pathname.startsWith(route)))
+            const isProtected = Boolean(protectedRoutes.find(route => pathname?.startsWith(route)))
 
             return isProtected
         }
@@ -25,6 +26,23 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
             { children }
         </ProtectedLayout>
     )
+
+    
+    if(["/", "/search/properties" ].includes(pathname)) {
+        return (
+            <PropertiesSSR queryParams={queryParams}>
+                <Layout>
+                    { children }
+                </Layout>
+            </PropertiesSSR>
+        )
+    }
+
+    const propertyRoutePattern = /^\/search\/properties\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/
+
+    if(propertyRoutePattern.test(pathname)) {
+       return children
+    }
 
     return (
         <Layout>
