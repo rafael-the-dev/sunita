95c4eec044b630257cf3a8fa019388a9d7b41243
Rafael Tivane <luislangabusiness@gmail.com>
2024-09-30 11:15:39 +0100

    Fix return type error

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -6,10 +6,15 @@ import Auth from "@/models/server/db/Auth";
 
 import { CredentialsType, LoginCredentialsType } from "@/types/login";
 
-export const POST = async (req: NextRequest): Promise<CredentialsType> => {
-    return apiHandler(req, async ({ mongoDbConfig }) => {
-        const { password, username } = await req.json() as LoginCredentialsType;
-        const credentials = await Auth.login({ password, username }, { mongoDbConfig });
-        return NextResponse.json(credentials);
-    });
+export const POST = async (req: NextRequest) => {
+    return apiHandler(
+        req, 
+        async ({ mongoDbConfig }) => {
+            const { password, username } = await req.json() as LoginCredentialsType;
+
+            const credentials = await Auth.login({ password, username }, { mongoDbConfig });
+
+            return NextResponse.json(credentials);
+        }
+    );
 };
