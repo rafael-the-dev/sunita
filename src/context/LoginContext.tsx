2c20036ff1d4903c05c9fd07e2f4bb8932d7528e
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-03 19:01:34 +0100

    Ident code

--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -30,103 +30,105 @@ export const LoginContextProvider = ({ children }: { children: React.ReactNode }
 
     const user = React.useMemo<UserType | {}>(() => !credentials ? {} : credentials.user, [ credentials ]);
 
-    const logout = React.useCallback(() => {
-        setCredentials(null)
-        router.push("/login")
-    }, [ router ])
+    const logout = React.useCallback(
+        () => {
+            setCredentials(null)
+            router.push("/login")
+        }, 
+        [ router ]
+    )
 
-    React.useEffect(() => {
-        try {
-            getItem<CredentialsType>("credentials")
-        } catch(e) {
-            configLocalStorage();
-        }
-    }, []);
-
-    React.useEffect(() => {
-        try {
-            if(credentials) {
-                setItem({ 
-                    key: "credentials", 
-                    value: credentials 
-                });
+    React.useEffect(
+        () => {
+            try {
+                getItem<CredentialsType>("credentials")
+            } catch(e) {
+                configLocalStorage();
             }
-        } catch(e) {
-        }
-    }, [ credentials ]);
+        }, 
+        []
+    );
+
+    React.useEffect(
+        () => {
+            try {
+                if(credentials) {
+                    setItem({ 
+                        key: "credentials", 
+                        value: credentials 
+                    });
+                }
+            } catch(e) {
+            }
+        }, 
+        [ credentials ]
+    );
 
     const isFirstRender = React.useRef(true)
 
-    const validateSavedToken = React.useCallback(async ({ signal }) => {
-        try {
-            const { access: { token } } = getItem<CredentialsType>("credentials");
+    const validateSavedToken = React.useCallback(
+        async ({ signal }) => {
+            try {
+                const { access: { token } } = getItem<CredentialsType>("credentials");
 
-            if(!token) throw new Error("Undefined local storage");
+                if(!token) throw new Error("Undefined local storage");
 
-            const options = {
-                headers: {
-                    authorization: `bearer ${token}`
-                },
-                signal
-            }
-            const res = await fetch(`/api/auth/refresh?token=${token}`, options);
+                const options = {
+                    headers: {
+                        authorization: `bearer ${token}`
+                    },
+                    signal
+                }
+                const res = await fetch(`/api/auth/refresh?token=${token}`, options);
 
-            if(res.status === 401) {
+                if(res.status === 401) {
+                    router.push("/login");
+                    return;
+                }
+
+                if(res.status !== 200) throw new Error("Refresh token error");
+
+                const result = (await res.json()) as CredentialsType;
+
+                const params = new URLSearchParams(window.location.search)
+                
+                setCredentials(result);
+
+                router.push(`${pathname}?${params.toString()}`)
+            } catch(e) {
+                console.error(e);
                 router.push("/login");
+            } finally {
+                setRevalidatingToken(false)
+            }
+        }, 
+        [ pathname, router ]
+    );
+
+    React.useEffect(
+        () => {
+            const controller = new AbortController();
+
+            if(isFirstRender.current) {
+                isFirstRender.current = false;
                 return;
             }
-
-            if(res.status !== 200) throw new Error("Refresh token error");
-
-            const result = (await res.json()) as CredentialsType;
-
-            const params = new URLSearchParams(window.location.search)
             
-            setCredentials(result);
+            validateSavedToken({ signal: controller.signal });
 
-            router.push(`${pathname}?${params.toString()}`)
-        } catch(e) {
-            console.error(e);
-            router.push("/login");
-        } finally {
-            setRevalidatingToken(false)
-        }
-    }, [ pathname, router ]);
-
-    /*const fetchPublicAccess = React.useCallback(
-        async (controller: AbortController) => {
-            try {
-                const res = await fetch("/api/auth/public-access", { signal: controller.signal });
-
-                const credentials = await res.json() as CredentialsType;
-
-                setCredentials(credentials);
-                setRevalidatingToken(false)
-            } catch(e) {
-                console.error(e)
-            }
-        },
-        []
-    )*/
-
-    React.useEffect(() => {
-        const controller = new AbortController();
-
-        if(isFirstRender.current) {
-            isFirstRender.current = false;
-            return;
-        }
-        
-        validateSavedToken({ signal: controller.signal });
-
-        return () => controller.abort();
-    }, [ validateSavedToken ]);
+            return () => controller.abort();
+        }, 
+        [ validateSavedToken ]
+    );
     
-    React.useEffect(() => {
-        if(!isPublicPath && !credentials && !revalidatingToken) {
-            router.push("/login")
-        }
-    }, [ credentials, isPublicPath, router, revalidatingToken ]);
+    React.useEffect(
+        () => {
+            if(!isPublicPath && !credentials && !revalidatingToken) {
+                router.push("/login")
+            }
+        }, 
+        [ credentials, isPublicPath, router, revalidatingToken ]
+    );
 
     return (
         <LoginContext.Provider
