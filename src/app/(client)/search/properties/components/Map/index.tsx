17d0d1a5ae2b70f3c47871e57d48e9915be7cc06
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-08 06:10:54 +0100

    Render markers based on getProperties data, get average latitude and longitude, and return an empty component, It map tab is not selected.

--- src/app/(client)/search/properties/components/Map/index.tsx
+++ src/app/(client)/search/properties/components/Map/index.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react"
+import { useCallback, useContext, useEffect, useMemo, useState } from "react"
 import classNames from "classnames"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import currency from "currency.js"
+import { LatLng } from "leaflet"
 
 import styles from "./styles.module.css"
 
 import { TABS } from "../Tabs/components/Tab/types"
+
+import { StoresContext } from "../../context"
 
 import useTab from "../Tabs/hooks/useTab"
 
@@ -19,38 +23,88 @@ type MarkerType = {
         long: number
     }
 }
-
+ 
 const PropertyMap = () => {
-    const [ markers, setMarkers ] = useState<MarkerType[]>([])
+    const { getProperties } = useContext(StoresContext);
 
-    const { isActive } = useTab()
+    const [ userLocation, setUserLocation ] = useState<MarkerType>(null);
+
+    const markers: MarkerType[] = useMemo(
+        () => {
+            const list = getProperties().map(property => {
+                return {
+                    description: property.name, 
+                    id: getId(),//currency(property?.address?.cords?.lat).add(property?.address?.cords?.long).value,
+                    cords: {
+                        lat: property?.address?.cords?.lat,
+                        long: property?.address?.cords?.long
+                    } 
+                }
+            });
+
+            if(userLocation) list.push(userLocation);
+            
+            return list;
+        },
+        [ getProperties, userLocation ]
+    )
+
+    const getCenter = useCallback(
+        () => {
+            const properties = getProperties()
+
+            const evaluatedLocations: number[] = []
+
+            const { lat, lng } = properties.reduce(
+                (prevValue, property) => {
+                    const newLocation = currency(property.address.cords.lat).add(prevValue.lat).value
+
+                    if(evaluatedLocations.includes(newLocation)) return prevValue
+
+                    evaluatedLocations.push(newLocation)
+
+                    return {
+                        lat: currency(property.address.cords.lat).add(prevValue.lat).value,
+                        lng: currency(property.address.cords.long).add(prevValue.lng).value
+                    }
+                },
+                { lat: 0, lng: 0 }
+            )
+
+            const avgLat = currency(lat).divide(properties.length).value;
+            const avgLng = currency(lng).divide(properties.length).value;
+
+            return new LatLng(avgLat, avgLng)
+        },
+        [ getProperties ]
+    )
+
+    const { isActive } = useTab();
 
     useEffect(
         () => {
             onGettingUserLocation(
-                (lat, long) => setMarkers(markers => {
-                    console.log(lat, long)
-                    return [
-                        ...markers, 
-                        { 
-                            description: "Your location", 
-                            id: getId(),
-                            cords: {
-                                lat,
-                                long
-                            } 
-                        }
-                    ]
+                (lat, long) => {
+                    setUserLocation({ 
+                        description: "Your location", 
+                        id: getId(),
+                        cords: {
+                            lat,
+                            long
+                        } 
+                    })
                 }
-            ))
+            )
         },
         []
     )
 
+    if(!isActive(TABS.MAP)) return <></>;
+
     return (
-        <div className={classNames(styles.mapContainer, { "hidden": !isActive(TABS.MAP)})}>
+        <div className={classNames(styles.mapContainer)}>
             <MapContainer 
-                center={[ -25.8998272, 32.5615616 ]} 
+                center={getCenter()} 
                 zoom={13} 
                 scrollWheelZoom={false}
                 style={{ height: "100%", width: "100%"}}>
