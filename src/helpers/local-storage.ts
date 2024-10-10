caa766b6943f36ee07657dd7a37d649f1d97dcad
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-10 06:36:12 +0100

    add language to local storaage type, then define local storage default values

--- src/helpers/local-storage.ts
+++ src/helpers/local-storage.ts
@@ -1,16 +1,29 @@
 
 import { CartType } from "@/types/cart";
 import { CredentialsType } from "@/types/login"
+import { LANGUAGE } from "@/types/language"
 import { ProductInfoType } from "@/types/product";
 
 type LocalStorageType = {
     credentials: CredentialsType;
     cart: CartType<ProductInfoType>;
+    language: LANGUAGE;
 };
 
-const LOCAL_STORAGE = "__my-app__";
+const LOCAL_STORAGE = "__sunita-booking-app__";
 
-export const configLocalStorage = () => localStorage.setItem(LOCAL_STORAGE, JSON.stringify({}))
+export const configLocalStorage = () => {
+    const defaultValues = {
+        credentials: {
+            access: {
+                token: ""
+            }
+        },
+        language: LANGUAGE.PORTUGUESE
+    }
+
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(defaultValues))
+}
 
 const get = (): LocalStorageType | null => JSON.parse(localStorage.getItem(LOCAL_STORAGE));
 
