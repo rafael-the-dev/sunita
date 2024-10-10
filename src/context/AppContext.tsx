9c600054c79048d3ff095dc9149e8d2c65d6fdfc
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-10 06:07:25 +0100

    Create language state and its change function

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -3,6 +3,8 @@
 import { FetchDataFuncType } from "@/hooks/useFetch/types";
 import * as React from "react";
 
+import { LANGUAGE } from "@/types/language"
+
 type DialogType = {
     header?: {
         onClose?: () => void;
@@ -14,9 +16,11 @@ type DialogType = {
 };
 
 type AppContextType = {
+    changeLanguage: (language: LANGUAGE) => void,
     dialog: DialogType | null,
     isLoading: React.MutableRefObject<boolean>;
-    fetchDataRef: React.MutableRefObject<FetchDataFuncType>
+    fetchDataRef: React.MutableRefObject<FetchDataFuncType>,
+    language: LANGUAGE
     setDialog: React.Dispatch<React.SetStateAction<DialogType>>
 };
 
@@ -24,16 +28,30 @@ export const AppContext = React.createContext<AppContextType | null>(null);
 
 export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
     const [ dialog, setDialog ] = React.useState<DialogType | null>(null);
+    const [ language, setLanguage ] = React.useState(LANGUAGE.PORTUGUESE)
 
     const isLoading = React.useRef(false);
     const fetchDataRef = React.useRef<FetchDataFuncType>(null)
 
+    const changeLanguage = React.useCallback(
+        (language: LANGUAGE) => {
+            const isValid = Object
+                .values(LANGUAGE)
+                .includes(language)
+
+            if(isValid) setLanguage(language)
+        },
+        []
+    )
+
     return (
         <AppContext.Provider
             value={{
+                changeLanguage,
                 dialog,
                 fetchDataRef,
                 isLoading,
+                language,
                 setDialog
             }}>
             { children }
