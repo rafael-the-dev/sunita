9c3e090576ddf9541f2db0fb5f415c1821a89292
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-15 11:55:52 +0100

    Convert file to typescript and add hasAccess field that tests if user has access to url or not

--- src/components/shared/side-bar/components/menu/index.jsx
+++ src/components/shared/side-bar/components/menu/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import classNames from "classnames"
 
 import styles from "./styles.module.css"
@@ -16,71 +16,90 @@ import ProductsIcon from "../../../../../../public/images/icons/milk-svgrepo-com
 
 import { LoginContext } from "@/context/LoginContext"
 
+import { hasStoreRoutesAccess, hasSystemPageAcess } from "@/middlewares/auth/pages/helpers"
+
 import ListItem from "./components/list-item";
 
 const Menu = () => {
     const { credentials, user } = useContext(LoginContext);
 
-    const list = [
-        {
-            icon: <HomeIcon />,
-            label: "Home",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }`
-        },
-        {
-            icon: <NewSaleIcon />,
-            label: "New sale",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales/new-sale`
-        },
-        {
-            icon: <SaleIcon />,
-            label: "Sales",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales`
-        },
-        {
-            icon: <ProductsIcon className={classNames(styles.productsIcon, "fill-current h-4 w-4 text-2xl")} />,
-            label: "Products",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/products`
-        },
-        {
-            icon: <BedroomIcon />,
-            label: "Rooms",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/rooms`
-        },
-        {
-            icon: <ExpensesIcon className={classNames(styles.expensesIcon)} />,
-            label: "Expenses",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/expenses`
-        },
-        {
-            icon: <UsersIcon />,
-            label: "Users",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/users`
-        },
-        {
-            icon: <FinancesIcon />,
-            label: "Finances",
-            path: `/stores/${ credentials?.user?.stores[0]?.storeId }/finances`
-        },
-        {
-            icon: <SystemIcon />,
-            label: "System",
-            path: `/system/stores`
-        },
-    ];
-
-    return (
-        <ul>
+    const list = useMemo(
+        () => [
             {
-                list.map((item, index) => (
-                    <ListItem 
-                        key={index}
-                        { ...item }
-                    />
-                ))
-            }
-        </ul>
+                hasAccess: hasStoreRoutesAccess,
+                icon: <HomeIcon />,
+                label: "Home",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <NewSaleIcon />,
+                label: "New sale",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales/new-sale`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <SaleIcon />,
+                label: "Sales",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <ProductsIcon className={classNames(styles.productsIcon, "fill-current h-4 w-4 text-2xl")} />,
+                label: "Products",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/products`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <BedroomIcon />,
+                label: "Rooms",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/rooms`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <ExpensesIcon className={classNames(styles.expensesIcon)} />,
+                label: "Expenses",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/expenses`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <UsersIcon />,
+                label: "Users",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/users`
+            },
+            {
+                hasAccess: hasStoreRoutesAccess,
+                icon: <FinancesIcon />,
+                label: "Finances",
+                path: `/stores/${ credentials?.user?.stores[0]?.storeId }/finances`
+            },
+            {
+                hasAccess: hasSystemPageAcess,
+                icon: <SystemIcon />,
+                label: "System",
+                path: `/system/stores`
+            },
+        ],
+        [ credentials ]
     );
+
+    const listMemo = useMemo(
+        () => (
+            <ul>
+                {
+                    list.map((item, index) => (
+                        <ListItem 
+                            key={index}
+                            { ...item }
+                        />
+                    ))
+                }
+        </ul>
+        ),
+        [ list ]
+    )
+
+    return listMemo;
 };
 
 export default Menu;
