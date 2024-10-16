033286c16ecad69d6300e0e35bb9ee1fffd09904
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-16 06:49:16 +0100

    translate menu to protuguese/english based on selected language

--- src/components/shared/side-bar/components/menu/index.jsx
+++ src/components/shared/side-bar/components/menu/index.jsx
@@ -14,13 +14,17 @@ import SystemIcon from '@mui/icons-material/AdminPanelSettings';
 
 import ProductsIcon from "../../../../../../public/images/icons/milk-svgrepo-com.svg"
 
+import { AppContext } from "@/context/AppContext"
 import { LoginContext } from "@/context/LoginContext"
+
+import lang from "./lang.json"
 
 import { hasStoreRoutesAccess, hasSystemPageAcess } from "@/middlewares/auth/pages/helpers"
 
 import ListItem from "./components/list-item";
 
 const Menu = () => {
+    const { language } = useContext(AppContext);
     const { credentials, user } = useContext(LoginContext);
 
     const list = useMemo(
@@ -28,59 +32,59 @@ const Menu = () => {
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <HomeIcon />,
-                label: "Home",
+                label: lang["home"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <NewSaleIcon />,
-                label: "New sale",
+                label: lang["new-sale"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales/new-sale`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <SaleIcon />,
-                label: "Sales",
+                label: lang["sales"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/sales`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <ProductsIcon className={classNames(styles.productsIcon, "fill-current h-4 w-4 text-2xl")} />,
-                label: "Products",
+                label: lang["products"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/products`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <BedroomIcon />,
-                label: "Rooms",
+                label: lang["properties"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/rooms`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <ExpensesIcon className={classNames(styles.expensesIcon)} />,
-                label: "Expenses",
+                label: lang["expenses"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/expenses`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <UsersIcon />,
-                label: "Users",
+                label: lang["users"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/users`
             },
             {
                 hasAccess: hasStoreRoutesAccess,
                 icon: <FinancesIcon />,
-                label: "Finances",
+                label: lang["finances"][language],
                 path: `/stores/${ credentials?.user?.stores[0]?.storeId }/finances`
             },
             {
                 hasAccess: hasSystemPageAcess,
                 icon: <SystemIcon />,
-                label: "System",
+                label: lang["System"][language],
                 path: `/system/stores`
             },
         ],
-        [ credentials ]
+        [ credentials, language ]
     );
 
     const listMemo = useMemo(
