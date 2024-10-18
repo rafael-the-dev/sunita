ef7cc42de4d4b47f0e961ec5b4feec33651629d2
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-18 11:01:13 +0100

    Create GET method handler

--- src/app/api/stores/[storeId]/route.ts
+++ src/app/api/stores/[storeId]/route.ts
@@ -1,10 +1,23 @@
 import { NextRequest, NextResponse } from "next/server"
 
+import { URLParamsType } from "@/types/app-config-server";
 import { Store } from "@/types/warehouse"
 
 import { apiHandler } from "@/middlewares/route-handler"
 
 import StoreModel from "@/models/server/db/Store"
+
+export const GET = async (req: NextRequest, { params: { storeId }}: URLParamsType) => {
+
+    return apiHandler(
+        req, 
+        async (config) => {
+            const store = await StoreModel.get(storeId, config)
+
+            return NextResponse.json(store)
+        }
+    )
+}
 
 export const PUT = async (req: NextRequest) => {
     const store = await req.json() as Store
