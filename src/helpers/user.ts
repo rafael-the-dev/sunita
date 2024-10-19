e29bd9ec14bddb6ecc8e7bc133b7e87f9c48611c
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-19 07:51:30 +0100

    get categories based on user's category

--- src/helpers/user.ts
+++ src/helpers/user.ts
@@ -1,6 +1,23 @@
 
-import { BaseUserType, USER_CATEGORY} from "@/types/user"
+import { CredentialsType } from "@/types/login"
+import { BaseUserType, USER_CATEGORY } from "@/types/user"
 
 export const isClientUser = (user: BaseUserType) => {
     return !user || user.category === USER_CATEGORY.CLIENT
+}
+
+export const getCategories = (credentials: CredentialsType) => {
+    const category = credentials?.user?.category
+
+    switch(category) {
+        case USER_CATEGORY.CLIENT: return Object.values(USER_CATEGORY.CLIENT)
+        case USER_CATEGORY.EMPLOYEE: return Object.values(USER_CATEGORY.EMPLOYEE)
+    }
+    
+    if([ USER_CATEGORY.ADMIN, USER_CATEGORY.MANAGER ].includes(category)) {
+        return Object.values([ USER_CATEGORY.EMPLOYEE, USER_CATEGORY.ADMIN, USER_CATEGORY.MANAGER ])
+    }
+
+    return Object
+        .values(USER_CATEGORY)
 }
