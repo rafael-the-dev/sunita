285aaab9a74aac27f9d0554b7dbcb40d21512ccb
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-19 07:32:03 +0100

    create update function

--- src/models/server/db/User/index.ts
+++ src/models/server/db/User/index.ts
@@ -1,11 +1,13 @@
 
 import { FiltersType } from "@/types/app-config-server";
 import { SettingsType } from "@/types/route";
-import { UserType } from "@/types/user";
+import { UserType, User } from "@/types/user";
 
 import { getUsers } from "./connections/db";
+import { getUserProxy } from "../../proxy/user";
 
 import Error404 from "@/errors/server/404Error";
+import InvalidArgumentError from "@/errors/server/InvalidArgumentError";
 
 class Users {
     
@@ -21,6 +23,70 @@ class Users {
         const users = await getUsers(filters, { mongoDbConfig, user });
     
         return users
+    }
+
+    static async update({ address, document, category, contact, firstName, lastName, username }: User, { mongoDbConfig, user }: SettingsType) {
+        let userDetails: UserType = await this.get({ username }, { mongoDbConfig })
+
+        try {
+            const userUpdates: User = {
+                address: null,
+                contact: null,
+                category: null,
+                document: null,
+                firstName: "",
+                id: "",
+                lastName: "",
+                password: null,
+                stores: [],
+                username: "",
+            };
+
+            const userProxy = getUserProxy(userUpdates);
+
+            userProxy.address = address;
+            userProxy.contact = contact
+            userProxy.document = document;
+            userProxy.category = category;
+            userProxy.firstName = firstName;
+            userProxy.lastName = lastName;
+            userProxy.username = username;
+
+            await mongoDbConfig
+                .collections
+                .USERS
+                .updateOne(
+                    { username },
+                    {
+                        $set: {
+                            address,
+                            contact, category,
+                            document,
+                            firstName,
+                            lastName
+                        }
+                    }
+                );
+        } catch(e) {
+            await mongoDbConfig
+                .collections
+                .USERS
+                .updateOne(
+                    { username },
+                    {
+                        $set: {
+                            address: userDetails.address,
+                            contact: userDetails.contact, 
+                            category: userDetails.category,
+                            document: userDetails.document,
+                            firstName: firstName,
+                            lastName: userDetails.lastName
+                        }
+                    }
+                );
+        }
+        
+        
     }
 }
 
