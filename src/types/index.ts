3167ca79a68108ec40fe634a6b4393741ea35ef3
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-21 07:27:09 +0100

    Create SerieType, and PERIOD enum

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,23 @@ export enum LATE_PAYMENT_FINE_PERIOD {
     YEAR = "y"
 }
 
+export enum PERIOD {
+    DAY = "DD",
+    WEEK = "ddd",
+    MONTH = "MMM",
+    YEAR = "YYYY"
+}
+
+export type SerieType = {
+    period: string,
+    totalAmount: number
+}
+
+export type ChartSerieType = {
+    list: SerieType[],
+    period: string
+}
+
 export enum STATUS {
     ACTIVE = "active",
     INACTIVE = "inactive",
