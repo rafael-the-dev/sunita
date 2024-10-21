ef3699c16be016430e40c51d44cc1e5a8036b709
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-21 07:30:23 +0100

    implement Chart component

--- src/app/stores/[storeId]/rooms/components/Booking/components/Chart/index.tsx
+++ src/app/stores/[storeId]/rooms/components/Booking/components/Chart/index.tsx
@@ -1,11 +1,12 @@
 
+import Chart from "./components/Chart"
 import Filters from "./components/Filters"
 
 const ChartContainer = () => {
-
     return (
         <div className="px-3">
             <Filters />
+            <Chart />
         </div>
     )
 }
