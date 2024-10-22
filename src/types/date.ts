c7ff17b40f96fd0216072b3b38dc8c7a9889fa92
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-22 15:23:45 +0100

    create MONTHS enum

---
+++ src/types/date.ts
@@ -0,0 +1,16 @@
+
+
+export enum MONTHS {
+    JAN = "Jan",
+    FEB = "Feb",
+    MAR = "Mar",
+    APR = "Apr",
+    MAY = "May",
+    JUN = "Jul",
+    JUL = "Jul",
+    AUG = "Aug",
+    SEP = "Sep",
+    OCT = "Oct",
+    NOV = "Nov",
+    DEC = "Dec",
+}
