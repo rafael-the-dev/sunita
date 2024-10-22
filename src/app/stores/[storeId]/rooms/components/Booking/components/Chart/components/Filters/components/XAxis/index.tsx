5cd3eac7af0fd0d34996c54fef6a055815e8d95f
Rafael Tivane <luislangabusiness@gmail.com>
2024-10-22 15:55:17 +0100

    Implement internalization in x axis

--- src/app/stores/[storeId]/rooms/components/Booking/components/Chart/components/Filters/components/XAxis/index.tsx
+++ src/app/stores/[storeId]/rooms/components/Booking/components/Chart/components/Filters/components/XAxis/index.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 import { X_AXIS } from "@/app/stores/[storeId]/rooms/components/Booking/components/Chart/components/Filters/types"
 
+import lang from "./lang.json"
+
+import useLanguage from "@/hooks/useLanguage"
 import useSearchParams from "@/hooks/useSearchParams"
 
 import RadioGroup from "@/common/components/RadioGroup"
@@ -9,12 +12,14 @@ import RadioGroup from "@/common/components/RadioGroup"
 const XAxis = () => {
     const searchParams = useSearchParams()
 
+    const { language } = useLanguage()
+
     const list = React.useRef(
         [
-            { label: "Day", value: X_AXIS.DAY },
-            { label: "Week", value: X_AXIS.WEEK },
-            { label: "Month", value: X_AXIS.MONTH },
-            { label: "Year", value: X_AXIS.YEAR },
+            { label: lang["day"][language], value: X_AXIS.DAY },
+            { label: lang["week"][language], value: X_AXIS.WEEK }, 
+            { label: lang["month"][language], value: X_AXIS.MONTH },
+            //{ label: "Year", value: X_AXIS.YEAR },
         ]
     )
 
